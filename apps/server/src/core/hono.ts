5024624840575b1667ec26e5b3f8539134c49270
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-09-13 06:28:10 +0700

    refactor: set repo as default context

--- apps/server/src/core/hono.ts
+++ apps/server/src/core/hono.ts
@@ -1,16 +1,19 @@
 import { Hono } from "hono";
+import type { DriverRepository } from "@/features/driver/repository";
 import type { AuthInstance } from "./services/auth";
 import type { DatabaseInstance } from "./services/db";
 import type { KeyValueService } from "./services/kv";
 
-export type HonoContext<ExtendVars extends Record<string, unknown>> = {
+export type HonoContext = {
 	Variables: {
 		db: DatabaseInstance;
 		auth: AuthInstance;
 		kv: KeyValueService;
-	} & ExtendVars;
+		repo: {
+			driver: DriverRepository;
+		};
+	};
 	Bindings: Env;
 };
 
-export const createHono = <Vars extends Record<string, unknown>>() =>
-	new Hono<HonoContext<Vars>>();
+export const createHono = () => new Hono<HonoContext>();
