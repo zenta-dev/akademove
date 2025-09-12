c361ea47851aafb35292349d3c7796151d85612d
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-09-12 21:31:52 +0700

    feat: enhance password handling with secure hashing and verification methods

--- apps/server/src/lib/services/auth.ts
+++ apps/server/src/lib/services/auth.ts
@@ -1,5 +1,6 @@
 import { env } from "cloudflare:workers";
-import { betterAuth } from "better-auth";
+import { scryptSync } from "node:crypto";
+import { BetterAuthError, betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import * as schema from "@/tables/auth";
 import type { DatabaseInstance } from "./db";
@@ -13,6 +14,48 @@ export const getAuth = (db: DatabaseInstance) =>
 		trustedOrigins: [env.CORS_ORIGIN],
 		emailAndPassword: {
 			enabled: true,
+			password: {
+				hash: async (password) => {
+					const salt = crypto.getRandomValues(new Uint8Array(16));
+					const saltHex = Array.from(salt)
+						.map((b) => b.toString(16).padStart(2, "0"))
+						.join("");
+
+					const key = scryptSync(password.normalize("NFKC"), saltHex, 64, {
+						N: 16384,
+						r: 16,
+						p: 1,
+						maxmem: 128 * 16384 * 16 * 2,
+					});
+
+					const keyHex = Array.from(key)
+						.map((b) => b.toString(16).padStart(2, "0"))
+						.join("");
+					return `${saltHex}:${keyHex}`;
+				},
+				verify: async ({ hash, password }) => {
+					const [saltHex, keyHex] = hash.split(":");
+					if (!saltHex || !keyHex) {
+						throw new BetterAuthError("Invalid password hash");
+					}
+					const targetKey = scryptSync(
+						password.normalize("NFKC"),
+						saltHex,
+						64,
+						{
+							N: 16384,
+							r: 16,
+							p: 1,
+							maxmem: 128 * 16384 * 16 * 2,
+						},
+					);
+
+					const targetKeyHex = Array.from(targetKey)
+						.map((b) => b.toString(16).padStart(2, "0"))
+						.join("");
+					return targetKeyHex === keyHex;
+				},
+			},
 		},
 		secret: env.AUTH_SECRET,
 		baseURL: env.AUTH_URL,
