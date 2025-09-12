9dd400a27df24fcaa19343f922938679f4a6acea
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-09-13 03:35:29 +0700

    feat: add generic type to extend hono factory

--- apps/server/src/core/hono.ts
+++ apps/server/src/core/hono.ts
@@ -1,13 +1,16 @@
 import { Hono } from "hono";
 import type { AuthInstance } from "./services/auth";
 import type { DatabaseInstance } from "./services/db";
+import type { KeyValueService } from "./services/kv";
 
-export type HonoContext = {
+export type HonoContext<ExtendVars extends Record<string, unknown>> = {
 	Variables: {
 		db: DatabaseInstance;
 		auth: AuthInstance;
-	};
+		kv: KeyValueService;
+	} & ExtendVars;
 	Bindings: Env;
 };
 
-export const createHono = () => new Hono<HonoContext>();
+export const createHono = <Vars extends Record<string, unknown>>() =>
+	new Hono<HonoContext<Vars>>();
