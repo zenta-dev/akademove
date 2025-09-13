a27b061f5c8a6e2cc3b14bf859dc7f928d09857d
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-09-13 08:37:18 +0700

    feat: set userId in require auth middleware

--- apps/server/src/core/middlewares/auth.ts
+++ apps/server/src/core/middlewares/auth.ts
@@ -1,20 +1,24 @@
 import { createMiddleware } from "hono/factory";
 import { MiddlewareError } from "../error";
+import type { HonoContext } from "../hono";
 
-export const requireAuthMiddleware = createMiddleware(async (c, next) => {
-	try {
-		const headers = new Headers();
-		for (const [k, v] of Object.entries(c.req.header())) {
-			headers.set(k, v);
+export const requireAuthMiddleware = createMiddleware<HonoContext>(
+	async (c, next) => {
+		try {
+			const headers = new Headers();
+			for (const [k, v] of Object.entries(c.req.header())) {
+				headers.set(k, v);
+			}
+			const session = await c.var.auth.api.getSession({ headers });
+			if (!session) {
+				throw new MiddlewareError("Failed to authenticate session");
+			}
+			c.set("userId", session.user.id);
+			await next();
+		} catch (error) {
+			throw new MiddlewareError("Failed to authenticate session", {
+				prevError: error instanceof Error ? error : undefined,
+			});
 		}
-		const session = await c.var.auth.api.getSession({ headers });
-		if (!session) {
-			throw new MiddlewareError("Failed to authenticate session");
-		}
-		await next();
-	} catch (error) {
-		throw new MiddlewareError("Failed to authenticate session", {
-			prevError: error instanceof Error ? error : undefined,
-		});
-	}
-});
+	},
+);
