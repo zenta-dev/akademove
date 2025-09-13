f73e31d87a501ee2132cbfe51bc5703d35f3465e
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-09-13 08:08:42 +0700

    feat: create validation error handler

--- apps/server/src/core/hono.ts
+++ apps/server/src/core/hono.ts
@@ -1,4 +1,5 @@
-import { Hono } from "hono";
+import { type Env, Hono, type ValidationTargets } from "hono";
+import type { StandardSchemaV1 } from "node_modules/zod/v4/core/standard-schema";
 import type { DriverRepository } from "@/features/driver/repository";
 import type { AuthInstance } from "./services/auth";
 import type { DatabaseInstance } from "./services/db";
@@ -17,3 +18,37 @@ export type HonoContext = {
 };
 
 export const createHono = () => new Hono<HonoContext>();
+
+export const handleValidation = <
+	Schema extends StandardSchemaV1,
+	Target extends keyof ValidationTargets,
+>(
+	result: (
+		| { success: true; data: StandardSchemaV1.InferOutput<Schema> }
+		| {
+				success: false;
+				error: readonly StandardSchemaV1.Issue[];
+				data: StandardSchemaV1.InferOutput<Schema>;
+		  }
+	) & { target: Target },
+) => {
+	if (!result.success) {
+		return new Response(
+			JSON.stringify({
+				success: false,
+				message: "Invalid request body",
+				errors: result.error.map((v) => ({
+					name: "ValidationError",
+					path: v.path?.at(0) ?? v.path,
+					cause: v.message,
+				})),
+			}),
+			{
+				status: 400,
+				headers: {
+					"Content-Type": "application/json",
+				},
+			},
+		);
+	}
+};
