928650017c70f77f5501799ea86a4a19b51a4d12
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-09-13 08:39:52 +0700

    feat: register driver feature

--- apps/server/src/features/handler.ts
+++ apps/server/src/features/handler.ts
@@ -1,4 +1,8 @@
 import { createHono } from "@/core/hono";
+import { DriverHandler } from "./driver/handler";
 
 const h = createHono();
+
+h.route("/drivers", DriverHandler);
+
 export { h as AppHandler };
