bd0a60628a521f4926dcb994624412d4bfdbe7b6
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-09-15 01:23:07 +0700

    feat: add initial oapi generator config

---
+++ apps/mobile/lib/oapi.dart
@@ -0,0 +1,17 @@
+// Openapi Generator last run: : 2025-09-15T01:20:02.970621
+import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
+
+@Openapi(
+  additionalProperties: DioProperties(
+    pubName: 'akademove_api',
+    pubAuthor: 'Rahmat Hidayatullah',
+  ),
+  inputSpec: RemoteSpec(
+    path: 'https://akademove-server.zenta.dev/openapi.json',
+  ),
+  generatorName: Generator.dio,
+  runSourceGenOnOutput: true,
+  outputDirectory: 'api/akademove_api',
+)
+// ignore: unused_element this for build only
+class _IgnoreMe {}
