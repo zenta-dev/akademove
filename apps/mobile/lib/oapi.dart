0a8ea377bdc43a9a483dcc12a897379ef79f7564
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-09-15 02:34:46 +0700

    chore: add auth oapi to generator config

--- apps/mobile/lib/oapi.dart
+++ apps/mobile/lib/oapi.dart
@@ -1,4 +1,4 @@
-// Openapi Generator last run: : 2025-09-15T01:20:02.970621
+// Openapi Generator last run: : 2025-09-15T02:20:10.717244
 import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
 
 @Openapi(
@@ -14,4 +14,19 @@ import 'package:openapi_generator_annotations/openapi_generator_annotations.dart
   outputDirectory: 'api/akademove_api',
 )
 // ignore: unused_element this for build only
-class _IgnoreMe {}
+class _AkadeMoveAPI {}
+
+@Openapi(
+  additionalProperties: DioProperties(
+    pubName: 'akademove_auth',
+    pubAuthor: 'Rahmat Hidayatullah',
+  ),
+  inputSpec: InputSpec(
+    path: 'auth-oapi.json',
+  ),
+  generatorName: Generator.dio,
+  runSourceGenOnOutput: true,
+  outputDirectory: 'api/akademove_auth',
+)
+// ignore: unused_element this for build only
+class _AkadeMoveAuth {}
