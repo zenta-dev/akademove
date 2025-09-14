7aa96e09a7ddf9204b4e6dd09aa59426494ce4f9
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-09-15 03:19:26 +0700

    chore: include generated oapi build

---
+++ gen/ts/api-client/src/apis/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './DriverApi';
+export * from './MerchantApi';
+export * from './OrderApi';
+export * from './PromoApi';
+export * from './ReportApi';
+export * from './ReviewApi';
+export * from './ScheduleApi';
