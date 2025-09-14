d6bbbbbbc30223798ddeb5b9a9cd6e6ff319f050
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-09-14 22:27:54 +0700

    feat: add review schema

---
+++ packages/schema/src/review.ts
@@ -0,0 +1,31 @@
+import * as z from "zod";
+import { DateSchema } from "./common.ts";
+import { CONSTANTS } from "./constants.ts";
+
+export const ReviewCategorySchema = z.enum(CONSTANTS.REVIEW_CATEGORIES);
+
+export const ReviewSchema = z.object({
+	id: z.uuid(),
+	orderId: z.uuid(),
+	fromUserId: z.string(),
+	toUserId: z.string(),
+	category: ReviewCategorySchema.default("other"),
+	score: z.number(),
+	comment: z.string().default(""),
+	createdAt: DateSchema,
+});
+
+export const InsertReviewSchema = ReviewSchema.omit({
+	id: true,
+	createdAt: true,
+});
+
+export const UpdateReviewSchema = ReviewSchema.omit({
+	id: true,
+	createdAt: true,
+});
+
+export type ReviewCategory = z.infer<typeof ReviewCategorySchema>;
+export type Review = z.infer<typeof ReviewSchema>;
+export type InsertReview = z.infer<typeof InsertReviewSchema>;
+export type UpdateReview = z.infer<typeof UpdateReviewSchema>;
