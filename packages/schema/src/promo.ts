b80d4965cce8eab0e80e07d3f25ee771a8f5268a
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-09-14 22:11:54 +0700

    feat: add promo schema

---
+++ packages/schema/src/promo.ts
@@ -0,0 +1,36 @@
+import * as z from "zod";
+import { DateSchema } from "./common.ts";
+
+export const PromoSchema = z.object({
+	id: z.uuid(),
+	name: z.string(),
+	code: z.string(),
+	rules: z.any(),
+	discountAmount: z.number(),
+	discountPercentage: z.number(),
+	minOrderAmount: z.number(),
+	maxOrderAmount: z.number(),
+	usageLimit: z.number(),
+	usedCount: z.number(),
+	periodStart: DateSchema,
+	periodEnd: DateSchema,
+	isActive: z.boolean(),
+	createdById: z.string(),
+	createdAt: DateSchema,
+});
+
+export const InsertPromoSchema = PromoSchema.omit({
+	id: true,
+	usedCount: true,
+	createdAt: true,
+});
+
+export const UpdatePromoSchema = PromoSchema.omit({
+	id: true,
+	usedCount: true,
+	createdAt: true,
+});
+
+export type Promo = z.infer<typeof PromoSchema>;
+export type InsertPromo = z.infer<typeof InsertPromoSchema>;
+export type UpdatePromo = z.infer<typeof UpdatePromoSchema>;
