0358b1a1305f1789393e1313a0ab4725c6eef9a5
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-09-15 23:16:01 +0700

    feat: fix token retrieval

--- apps/web/src/lib/api-client.ts
+++ apps/web/src/lib/api-client.ts
@@ -1,21 +1,28 @@
 import { QueryCache, QueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 import { client } from "@/api/client.gen";
 
-export async function setupApiClient() {
+export function setupApiClient() {
 	if (typeof window !== "undefined") {
 		client.setConfig({
 			baseUrl: import.meta.env.VITE_SERVER_URL,
-			auth: async () => {
-				return `Bearer ${localStorage.getItem("bearer")}`;
+			auth: () => {
+				return (
+					localStorage.getItem("bearer_token") ||
+					localStorage.getItem("jwt") ||
+					""
+				);
 			},
 		});
 	} else {
-		const { getHeaders } = await import("@tanstack/react-start/server");
-		client.setConfig({
-			baseUrl: import.meta.env.VITE_SERVER_URL,
-			headers: getHeaders(),
+		import("@tanstack/react-start/server").then(({ getHeaders }) => {
+			client.setConfig({
+				baseUrl: import.meta.env.VITE_SERVER_URL,
+				headers: getHeaders(),
+			});
 		});
 	}
+	return client;
 }
 
 export const queryClient = new QueryClient({
