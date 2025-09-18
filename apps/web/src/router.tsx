456dad334f0f9c6cc2d745185632d94292c6e635
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-09-18 08:06:13 +0700

    fix: use intent preload into router

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -1,5 +1,4 @@
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
-import Loader from "./components/loader";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/api-client";
@@ -8,10 +7,10 @@ import { routeTree } from "./routeTree.gen";
 export const createRouter = () => {
 	const router = createTanStackRouter({
 		routeTree,
+		defaultPreload: "intent",
 		scrollRestoration: true,
 		defaultPreloadStaleTime: 0,
 		context: { locale: "en", queryClient },
-		defaultPendingComponent: () => <Loader />,
 		defaultNotFoundComponent: () => <div>Not Found</div>,
 		Wrap: ({ children }) => (
 			<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
