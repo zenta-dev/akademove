8e905624cc2c08ecd5679dab88a077b8f423afea
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-09-18 09:08:46 +0700

    feat: create basic public header

---
+++ apps/web/src/components/header/public.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/utils/cn";
+import { UserDropdwon } from "./user-dropdown";
+
+export const PublicHeader = ({
+	className,
+	...props
+}: React.ComponentProps<"header">) => {
+	return (
+		<header
+			className={cn("sticky top-0 z-50 flex w-full items-center", className)}
+			{...props}
+		>
+			<div className="flex h-(--header-height) w-full items-center justify-between px-2">
+				<div className="flex h-(--header-height) items-center gap-[7px]">
+					<h1 className="font-semibold text-xl">
+						<span className="hidden md:inline">AkadeMove</span>
+					</h1>
+				</div>
+				<UserDropdwon />
+			</div>
+		</header>
+	);
+};
