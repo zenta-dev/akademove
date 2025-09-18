c532174e62bb56b21e09cc1296c4df2dca91310d
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-09-18 08:00:09 +0700

    feat: add user management schema

--- packages/schema/src/user.ts
+++ packages/schema/src/user.ts
@@ -1,13 +1,33 @@
+import { m } from "@repo/i18n";
 import * as z from "zod";
 import { DateSchema } from "./common.ts";
+
+export const UserRoleSchema = z
+	.union([
+		z.literal("admin"),
+		z.literal("operator"),
+		z.literal("merchant"),
+		z.literal("driver"),
+		z.literal("user"),
+	])
+	.meta({
+		title: "UserRole",
+		ref: "UserRole",
+	});
 
 export const UserSchema = z
 	.object({
 		id: z.string(),
-		name: z.string(),
-		email: z.email(),
+		name: z.string().min(1, m.required_placeholder({ field: m.name() })),
+		email: z.email(
+			m.invalid_placeholder({ field: m.email_address().toLowerCase() }),
+		),
 		emailVerified: z.boolean().default(false),
 		image: z.url().nullable(),
+		role: UserRoleSchema,
+		banned: z.boolean().default(false),
+		banReason: z.string().nullable(),
+		banExpires: DateSchema.nullable(),
 		createdAt: DateSchema,
 		updatedAt: DateSchema,
 	})
@@ -18,16 +38,63 @@ export const UserSchema = z
 
 export const InsertUserSchema = UserSchema.omit({
 	id: true,
+	emailVerified: true,
+	image: true,
+	banned: true,
+	banReason: true,
+	banExpires: true,
 	createdAt: true,
 	updatedAt: true,
+})
+	.extend({
+		password: z
+			.string()
+			.min(8, m.min_placeholder({ field: m.password(), min: 8 })),
+		confirmPassword: z
+			.string()
+			.min(8, m.min_placeholder({ field: m.confirm_password(), min: 8 })),
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		path: ["confirmPassword"],
+		message: m.password_do_not_match(),
+	});
+
+export const UpdateUserRoleSchema = UserSchema.pick({ role: true }).meta({
+	title: "UpdateUserRoleRequest",
 });
 
-export const UpdateUserSchema = UserSchema.omit({
-	id: true,
-	createdAt: true,
-	updatedAt: true,
+export const UpdateUserPasswordSchema = InsertUserSchema.pick({
+	password: true,
+	confirmPassword: true,
+}).meta({
+	title: "UpdateUserPasswordRequest",
 });
 
+export const BanUserSchema = z
+	.object({
+		banReason: z.string(),
+		banExpiresIn: z.number().optional(),
+	})
+	.meta({
+		title: "BanUserSchemaRequest",
+	});
+
+export const UnbanUserSchema = UserSchema.pick({ id: true }).meta({
+	title: "UnbanUserSchemaRequest",
+});
+
+export const UpdateUserSchema = z.union([
+	UpdateUserRoleSchema,
+	UpdateUserPasswordSchema,
+	BanUserSchema,
+	UnbanUserSchema,
+]);
+
+export type UserRole = z.infer<typeof UserRoleSchema>;
 export type User = z.infer<typeof UserSchema>;
 export type InsertUser = z.infer<typeof InsertUserSchema>;
+export type UpdateUserRole = z.infer<typeof UpdateUserRoleSchema>;
+export type UpdateUserPassword = z.infer<typeof UpdateUserPasswordSchema>;
+export type BanUser = z.infer<typeof BanUserSchema>;
+export type UnbanUser = z.infer<typeof UnbanUserSchema>;
 export type UpdateUser = z.infer<typeof UpdateUserSchema>;
