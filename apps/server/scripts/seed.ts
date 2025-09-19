178b3fbde79bcc48f36e50c33ede3d845b6bad63
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-09-20 00:06:13 +0700

    feat: implement manual seeding

--- apps/server/scripts/seed.ts
+++ apps/server/scripts/seed.ts
@@ -1,9 +1,18 @@
+import { scryptSync } from "node:crypto";
 import * as readline from "node:readline";
+import type { InsertUser } from "@repo/schema/user";
+import { authPermission } from "@repo/shared";
+import { BetterAuthError, betterAuth, email } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { admin } from "better-auth/plugins";
+import { eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/postgres-js";
-import { seed } from "drizzle-seed";
 import postgres from "postgres";
-
-import * as auth from "@/core/tables/auth";
+import * as schema from "@/core/tables/auth";
+import {
+	type ConfigurationDatabase,
+	configuration,
+} from "@/core/tables/configuration";
 import * as driver from "@/core/tables/driver";
 import * as merchant from "@/core/tables/merchant";
 import * as order from "@/core/tables/order";
@@ -38,32 +47,160 @@ async function confirmExecution() {
 		process.exit(0);
 	}
 }
+await confirmExecution();
 
-async function main() {
-	await confirmExecution();
+const client = postgres(process.env.DATABASE_URL || "");
+const db = drizzle({ client });
+const auth = betterAuth({
+	database: drizzleAdapter(db, {
+		provider: "pg",
+		schema: schema,
+	}),
+	emailAndPassword: {
+		enabled: true,
+		password: {
+			hash: async (password) => {
+				const salt = crypto.getRandomValues(new Uint8Array(16));
+				const saltHex = Array.from(salt)
+					.map((b) => b.toString(16).padStart(2, "0"))
+					.join("");
 
-	const client = postgres(process.env.DATABASE_URL || "");
-	const db = drizzle({ client });
+				const key = scryptSync(password.normalize("NFKC"), saltHex, 64, {
+					N: 16384,
+					r: 16,
+					p: 1,
+					maxmem: 128 * 16384 * 16 * 2,
+				});
 
-	await seed(
-		db,
-		{
-			...auth,
-			...driver,
-			...merchant,
-			...order,
-			...promo,
-			...report,
-			...review,
-			...schedule,
+				const keyHex = Array.from(key)
+					.map((b) => b.toString(16).padStart(2, "0"))
+					.join("");
+				return `${saltHex}:${keyHex}`;
+			},
+			verify: async ({ hash, password }) => {
+				const [saltHex, keyHex] = hash.split(":");
+				if (!saltHex || !keyHex) {
+					throw new BetterAuthError("Invalid password hash");
+				}
+				const targetKey = scryptSync(password.normalize("NFKC"), saltHex, 64, {
+					N: 16384,
+					r: 16,
+					p: 1,
+					maxmem: 128 * 16384 * 16 * 2,
+				});
+
+				const targetKeyHex = Array.from(targetKey)
+					.map((b) => b.toString(16).padStart(2, "0"))
+					.join("");
+				return targetKeyHex === keyHex;
+			},
 		},
-		{ count: 100 },
-	);
+	},
+	plugins: [admin(authPermission)],
+});
 
-	console.log("✅ Database seeded with test data.");
+async function seedUser() {
+	const _USER: Omit<InsertUser, "confirmPassword">[] = [
+		{
+			name: "Test Admin 1",
+			email: "test-admin-1@akademove.com",
+			role: "admin",
+			password: "Ch@ngEThi5",
+		},
+		{
+			name: "Test Operator 1",
+			email: "test-operator-1@akademove.com",
+			role: "operator",
+			password: "Ch@ngEThi5",
+		},
+		{
+			name: "Test Merchant 1",
+			email: "test-merchant-1@akademove.com",
+			role: "merchant",
+			password: "Ch@ngEThi5",
+		},
+		{
+			name: "Test Driver 1",
+			email: "test-driver-1@akademove.com",
+			role: "driver",
+			password: "Ch@ngEThi5",
+		},
+		{
+			name: "Test User 1",
+			email: "test-user-1@akademove.com",
+			role: "user",
+			password: "Ch@ngEThi5",
+		},
+	];
+
+	const promises = [];
+	for (const user of _USER) {
+		const find = await db
+			.select()
+			.from(schema.user)
+			.where(eq(schema.user.email, user.email));
+		if (find.length > 0) continue;
+		promises.push(auth.api.createUser({ body: user }));
+	}
+	console.log(`SEEDING ${promises.length} USER`);
+	await Promise.all(promises);
 }
 
-main().catch((err) => {
-	console.error("❌ Error during seeding:", err);
-	process.exit(1);
-});
+async function seedConfigurations() {
+	console.log("Seeding Configurations");
+	const _UPDATER_ID = "ize5yQd0uPJSZliK3vXZKxxdyRww9DCF"; // should pinning to master admin
+
+	const _CONFIGS: ConfigurationDatabase[] = [
+		{
+			key: "ride-service-pricing",
+			name: "Ride Pricing",
+			value: {
+				price_per_km: 5000,
+				commission: 7,
+			},
+			description: "Current base price per km",
+			updatedById: _UPDATER_ID,
+			updatedAt: new Date(),
+		},
+		{
+			key: "delivery-service-pricing",
+			name: "Delivery Pricing",
+			value: {
+				price_per_km: 4500,
+				commission: 5,
+			},
+			description: "Current base price per km",
+			updatedById: _UPDATER_ID,
+			updatedAt: new Date(),
+		},
+		{
+			key: "food-service-pricing",
+			name: "Food Pricing",
+			value: {
+				price_per_km: 4000,
+				commission: 3,
+			},
+			description: "Current base price per km",
+			updatedById: _UPDATER_ID,
+			updatedAt: new Date(),
+		},
+	];
+	for (const conf of _CONFIGS) {
+		console.log("FIND CONFIG WITH KEY ", conf.key);
+		const find = await db
+			.select()
+			.from(configuration)
+			.where(eq(configuration.key, conf.key));
+		if (find.length > 0) {
+			console.log(find);
+			continue;
+		}
+		const res = await db.insert(configuration).values(conf).returning();
+		console.log(res);
+	}
+}
+
+await seedUser();
+await seedConfigurations();
+console.log("✅ Database seeded with test data.");
+process.exit();
