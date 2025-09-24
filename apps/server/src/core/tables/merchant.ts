e45f4f997eeb09d66016371ff062cda60d6a97c4
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-09-24 12:26:13 +0700

    feat: create merchant menu table & schema

--- apps/server/src/core/tables/merchant.ts
+++ apps/server/src/core/tables/merchant.ts
@@ -4,12 +4,14 @@ import { relations } from "drizzle-orm";
 import {
 	boolean,
 	decimal,
+	integer,
 	jsonb,
 	pgEnum,
 	pgTable,
 	text,
 	timestamp,
 	uuid,
+	varchar,
 } from "drizzle-orm/pg-core";
 import { user } from "./auth";
 
@@ -32,14 +34,40 @@ export const merchant = pgTable("merchants", {
 	updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
 
+export const merchantMenu = pgTable("merchant_menus", {
+	id: uuid().primaryKey().defaultRandom(),
+	merchantId: uuid()
+		.notNull()
+		.references(() => merchant.id, { onDelete: "cascade" }),
+	name: varchar({ length: 255 }).notNull(),
+	category: varchar({ length: 255 }),
+	price: decimal("base_price", {
+		precision: 10,
+		scale: 2,
+		mode: "number",
+	}).notNull(),
+	stock: integer().notNull(),
+	createdAt: timestamp("created_at").notNull().defaultNow(),
+	updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
+
 ///
 /// --- Relations --- ///
 ///
-export const merchantRelations = relations(merchant, ({ one }) => ({
+export const merchantRelations = relations(merchant, ({ one, many }) => ({
 	user: one(user, {
 		fields: [merchant.userId],
 		references: [user.id],
 	}),
+	menus: many(merchantMenu),
+}));
+
+export const merchantMenuRelations = relations(merchantMenu, ({ one }) => ({
+	merchant: one(merchant, {
+		fields: [merchantMenu.merchantId],
+		references: [merchant.id],
+	}),
 }));
 
 export type MerchantDatabase = typeof merchant.$inferSelect;
+export type MerchantMenuDatabase = typeof merchantMenu.$inferSelect;
--- packages/schema/src/merchant.ts
+++ packages/schema/src/merchant.ts
@@ -21,6 +21,16 @@ export const MerchantSchema = z
 		title: "Merchant",
 		ref: "Merchant",
 	});
+export const MerchantMenuSchema = z.object({
+	id: z.uuid(),
+	merchantId: z.uuid(),
+	name: z.string(),
+	category: z.string().optional(),
+	price: z.number(),
+	stock: z.number(),
+	createdAt: DateSchema,
+	updatedAt: DateSchema,
+});
 
 export const InsertMerchantSchema = MerchantSchema.omit({
 	id: true,
@@ -29,10 +39,19 @@ export const InsertMerchantSchema = MerchantSchema.omit({
 	createdAt: true,
 	updatedAt: true,
 });
+export const InsertMerchantMenuSchema = MerchantMenuSchema.omit({
+	id: true,
+	createdAt: true,
+	updatedAt: true,
+});
 
 export const UpdateMerchantSchema = InsertMerchantSchema.partial();
+export const UpdateMerchantMenuSchema = InsertMerchantMenuSchema.partial();
 
 export type MerchantType = z.infer<typeof MerchantTypeSchema>;
 export type Merchant = z.infer<typeof MerchantSchema>;
+export type MerchantMenu = z.infer<typeof MerchantMenuSchema>;
 export type InsertMerchant = z.infer<typeof InsertMerchantSchema>;
+export type InsertMerchantMenu = z.infer<typeof InsertMerchantMenuSchema>;
 export type UpdateMerchant = z.infer<typeof UpdateMerchantSchema>;
+export type UpdateMerchantMenu = z.infer<typeof UpdateMerchantMenuSchema>;
