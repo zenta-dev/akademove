36a30e5d0b71c4706174907b1906cc433ced9af1
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-10-01 06:40:06 +0700

    feat: add auth and rbac middleware

--- apps/server/src/features/configuration/handler.ts
+++ apps/server/src/features/configuration/handler.ts
@@ -1,35 +1,44 @@
 import { implement } from "@orpc/server";
+import { authMiddleware, hasPermission } from "@/core/middlewares/auth";
 import type { ORPCCOntext } from "@/core/orpc";
 import { ConfigurationSpec } from "./spec";
 
-const os = implement(ConfigurationSpec).$context<ORPCCOntext>();
+const os = implement(ConfigurationSpec)
+	.$context<ORPCCOntext>()
+	.use(authMiddleware);
 
 export const ConfigurationHandler = os.router({
-	list: os.list.handler(async ({ context, input: { query } }) => {
-		const result = await context.repo.configuration.list(query);
-		return {
-			status: 200,
-			body: {
-				message: "Successfully retrieved configurations data",
-				data: result,
-			},
-		};
-	}),
-	get: os.get.handler(async ({ context, input: { params } }) => {
-		const result = await context.repo.configuration.get(params.key);
-		return {
-			status: 200,
-			body: {
-				message: "Successfully retrieved configuration data",
-				data: result,
-			},
-		};
-	}),
-	update: os.update.handler(async ({ context, input: { params, body } }) => {
-		const result = await context.repo.configuration.update(params.key, body);
-		return {
-			status: 200,
-			body: { message: "Configuration updated successfully", data: result },
-		};
-	}),
+	list: os.list
+		.use(hasPermission({ configurations: ["list"] }))
+		.handler(async ({ context, input: { query } }) => {
+			const result = await context.repo.configuration.list(query);
+			return {
+				status: 200,
+				body: {
+					message: "Successfully retrieved configurations data",
+					data: result,
+				},
+			};
+		}),
+	get: os.get
+		.use(hasPermission({ configurations: ["get"] }))
+		.handler(async ({ context, input: { params } }) => {
+			const result = await context.repo.configuration.get(params.key);
+			return {
+				status: 200,
+				body: {
+					message: "Successfully retrieved configuration data",
+					data: result,
+				},
+			};
+		}),
+	update: os.update
+		.use(hasPermission({ configurations: ["update"] }))
+		.handler(async ({ context, input: { params, body } }) => {
+			const result = await context.repo.configuration.update(params.key, body);
+			return {
+				status: 200,
+				body: { message: "Configuration updated successfully", data: result },
+			};
+		}),
 });
