b2fc72470a00e71ca8acc996fc630b0e8af326c5
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-09-30 14:49:30 +0700

    feat: create context helper

--- apps/mobile/lib/core/helpers.dart
+++ apps/mobile/lib/core/helpers.dart
@@ -1,5 +1,12 @@
+import 'package:flutter/material.dart';
+
 void noop() {}
 
 Future<void> delay(Duration duration) async {
   await Future.delayed(duration, noop);
+}
+
+extension BuildContextExt on BuildContext {
+  ThemeData get theme => Theme.of(this);
+  bool get isDarkMode => theme.brightness == Brightness.dark;
 }
