08c08bbf2d5022e185bd801553fc7f9d1c724560
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-10-11 06:34:45 +0700

    feat: create get file extension helper

--- packages/shared/src/string.ts
+++ packages/shared/src/string.ts
@@ -1,3 +1,8 @@
 export function capitalizeFirstLetter(string: string): string {
 	return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+export function getFileExtension(file: File): string {
+	const match = file.name.match(/\.([^.]+)$/);
+	return match ? (match[1] ?? "png").toLowerCase() : "";
+}
