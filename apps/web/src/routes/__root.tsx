a109c49bcdfb6090ff40812df93d5043feea4d65
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-10-11 06:58:32 +0700

    style: update root layout

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -6,11 +6,13 @@ import {
 	HeadContent,
 	Outlet,
 	Scripts,
+	useRouterState,
 } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { ThemeProvider } from "@/components/providers/theme";
 import { Toaster } from "@/components/ui/sonner";
 import { APP_NAME } from "@/lib/constants";
+import { cn } from "@/utils/cn";
 import appCss from "../index.css?url";
 
 export type RouterAppContext = {
@@ -43,21 +45,27 @@ export const Route = createRootRouteWithContext<RouterAppContext>()({
 });
 
 function RootDocument() {
+	const routerState = useRouterState();
+
 	return (
 		<html lang={getLocale()} suppressHydrationWarning>
 			<head>
 				<HeadContent />
 			</head>
-			<body className="h-svh w-svw">
+			<body
+				className={
+					cn("h-screen", routerState.location.pathname.includes("dash"))
+						? "w-full"
+						: "w-[99.2vw]"
+				}
+			>
 				<ThemeProvider
 					attribute="class"
 					defaultTheme="system"
 					enableSystem
 					disableTransitionOnChange
 				>
-					<div className="grid h-svh grid-rows-[auto_1fr] bg-muted/50 dark:bg-muted/10">
-						<Outlet />
-					</div>
+					<Outlet />
 					<Toaster richColors />
 				</ThemeProvider>
 				<TanStackRouterDevtools position="bottom-left" />
