14e771cae20cf05a1666fb3c4b00b6ced01fff5f
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-10-11 06:43:11 +0700

    feat: create sign up dialog

---
+++ apps/web/src/components/dialogs/sign-up-dialog.tsx
@@ -0,0 +1,86 @@
+import { m } from "@repo/i18n";
+import { Link } from "@tanstack/react-router";
+import { type FC, type ReactNode, useState } from "react";
+import DriverChar from "@/assets/character/driver.svg?url";
+import MerchantChar from "@/assets/character/merchant.svg?url";
+import UserChar from "@/assets/character/user.svg?url";
+import type { FileRouteTypes } from "@/routeTree.gen";
+import { Card, CardContent } from "../ui/card";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "../ui/dialog";
+
+interface SignUpDialogProps {
+	asChild?: boolean;
+	children?: ReactNode;
+}
+
+const ROUTES: Array<{
+	name: string;
+	desc: string;
+	image: string;
+	path: FileRouteTypes["to"];
+}> = [
+	{
+		name: m.user(),
+		desc: m.sign_up_dialog_user_desc(),
+		image: UserChar,
+		path: "/{-$lang}/sign-up/user",
+	},
+	{
+		name: m.driver(),
+		desc: m.sign_up_dialog_driver_desc(),
+		image: DriverChar,
+		path: "/{-$lang}/sign-up/driver",
+	},
+	{
+		name: m.merchant(),
+		desc: m.sign_up_dialog_merchant_desc(),
+		image: MerchantChar,
+		path: "/{-$lang}/sign-up/merchant",
+	},
+];
+
+export const SignUpDialog: FC<SignUpDialogProps> = ({ asChild, children }) => {
+	const [open, setOpen] = useState(false);
+	return (
+		<Dialog open={open} onOpenChange={setOpen}>
+			<DialogTrigger asChild={asChild} className="cursor-pointer">
+				{asChild ? children : "Sign Up"}
+			</DialogTrigger>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>{m.sign_up_dialog()}</DialogTitle>
+					<DialogDescription>{m.sign_up_dialog_desc()}</DialogDescription>
+				</DialogHeader>
+				<div className="flex flex-col items-center gap-4">
+					{ROUTES.map(({ name, desc, image: AssetImage, path }) => (
+						<Link
+							key={name}
+							to={path}
+							className="w-full"
+							onClick={() => setOpen(false)}
+						>
+							<Card className="w-full transition-all hover:scale-105 hover:shadow-md">
+								<CardContent className="flex w-full flex-col items-center gap-2">
+									<img src={AssetImage} alt={name} className="size-20" />
+									<div className="w-full">
+										<p className="text-center font-medium text-;g">{name}</p>
+										<p className="text-center text-muted-foreground text-sm">
+											{desc}
+										</p>
+									</div>
+								</CardContent>
+							</Card>
+						</Link>
+					))}
+				</div>
+			</DialogContent>
+		</Dialog>
+	);
+};
