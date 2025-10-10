5740be7baad6aee0d7c9df09f3ff054238413b0b
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-10-11 06:44:25 +0700

    fix: redirect to role based dashboard

--- apps/web/src/components/header/dashboard-navigator.tsx
+++ apps/web/src/components/header/dashboard-navigator.tsx
@@ -1,68 +1,23 @@
-import { localizeHref, m } from "@repo/i18n";
+import { localizeHref } from "@repo/i18n";
+import type { UserRole } from "@repo/schema/user";
 import { Link } from "@tanstack/react-router";
-import {
-	GripIcon,
-	SettingsIcon,
-	ShieldIcon,
-	StoreIcon,
-	TruckIcon,
-	UserIcon,
-} from "lucide-react";
-import { Button } from "../ui/button";
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuTrigger,
-} from "../ui/dropdown-menu";
+import { GripIcon } from "lucide-react";
+import { useMemo } from "react";
+import { buttonVariants } from "../ui/button";
 
-// TODO: SHOW BASED USER ROLE
-const ROUTES = [
-	{
-		name: m.administrator(),
-		href: localizeHref("/dash/admin"),
-		icon: <ShieldIcon className="mr-2 h-4 w-4" />,
-	},
-	{
-		name: m.operator(),
-		href: localizeHref("/dash/operator"),
-		icon: <SettingsIcon className="mr-2 h-4 w-4" />,
-	},
-	{
-		name: m.merchant(),
-		href: localizeHref("/dash/merchant"),
-		icon: <StoreIcon className="mr-2 h-4 w-4" />,
-	},
-	{
-		name: m.driver(),
-		href: localizeHref("/dash/driver"),
-		icon: <TruckIcon className="mr-2 h-4 w-4" />,
-	},
-	{
-		name: m.user(),
-		href: localizeHref("/dash/user"),
-		icon: <UserIcon className="mr-2 h-4 w-4" />,
-	},
-];
+export const DashboardNavigator = ({ role }: { role: UserRole }) => {
+	const route = useMemo(() => {
+		return localizeHref(`/dash/${role}`);
+	}, [role]);
 
-export const DashboardNavigator = () => {
+	if (role === "user") return;
+
 	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<Button variant="ghost" size="icon">
-					<GripIcon />
-				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent align="end">
-				{ROUTES.map((e) => (
-					<Link key={e.name} to={e.href} className="justify-end">
-						<DropdownMenuItem>
-							{e.icon}
-							{e.name}
-						</DropdownMenuItem>
-					</Link>
-				))}
-			</DropdownMenuContent>
-		</DropdownMenu>
+		<Link
+			to={route}
+			className={buttonVariants({ variant: "ghost", size: "icon" })}
+		>
+			<GripIcon />
+		</Link>
 	);
 };
