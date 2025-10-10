9c99035b6882b7baab95b3d4aa218b9622d38d34
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-10-11 06:50:22 +0700

    feat: update orpc cache

--- apps/web/src/lib/client/orpc.tsx
+++ apps/web/src/lib/client/orpc.tsx
@@ -1,16 +1,20 @@
-import { createORPCClient, onError } from "@orpc/client";
+import { createORPCClient, ORPCError, onError, onStart } from "@orpc/client";
 import { RPCLink } from "@orpc/client/fetch";
 import { createTanstackQueryUtils } from "@orpc/tanstack-query";
+import { getAuthToken } from "@repo/shared";
 import { QueryCache, QueryClient } from "@tanstack/react-query";
 import { RefreshCwIcon } from "lucide-react";
 import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
+import { isDev } from "@/utils";
+import { log } from "@/utils/logger";
 import type { ServerSpecClient } from "../../../../server/src/features/index";
 
 export const queryClient = new QueryClient({
 	queryCache: new QueryCache({
 		onError: (error) => {
-			if (error.message.includes("my-session")) return;
+			if (error.message.toLowerCase().includes("session")) return;
+			if (error.message.toLowerCase().includes("token")) return;
 			toast.error(`Error: ${error.message}`, {
 				action: {
 					label: (
@@ -29,15 +33,35 @@ export const queryClient = new QueryClient({
 
 export const link = new RPCLink({
 	url: `${import.meta.env.VITE_SERVER_URL}/rpc`,
-	fetch(request, options) {
+	method: ({ context }, path) => {
+		if (context?.cache) {
+			return "GET";
+		}
+		if (typeof window === "undefined") {
+			return "GET";
+		}
+		if (path.at(-1)?.match(/^(?:get|find|list|search)(?:[A-Z].*)?$/)) {
+			return "GET";
+		}
+		if (path.at(-1)?.toLowerCase().includes("get")) {
+			return "GET";
+		}
+		return "POST";
+	},
+	fetch(request, options, { context }) {
 		return fetch(request, {
 			...options,
 			credentials: "include",
+			cache: context?.cache,
 		});
 	},
 	adapterInterceptors: [
+		async (c) => {
+			c.request.headers.set("X-Client-Agent", "web");
+			return await c.next();
+		},
 		onError((error) => {
-			console.error("ORPC ERROR", error);
+			log.error({ error }, "RPC ERROR");
 		}),
 	],
 });
