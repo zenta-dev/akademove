8ff341fc0b988061a5dac2c7495c180fd8dff912
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-10-11 06:55:44 +0700

    chore: update new tanstack router defintion

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -4,20 +4,21 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/client/orpc";
 import { routeTree } from "./routeTree.gen";
 
-export const router = createTanStackRouter({
-	routeTree,
-	defaultPreload: "intent",
-	scrollRestoration: true,
-	defaultPreloadStaleTime: 0,
-	context: { locale: "en", queryClient },
-	defaultNotFoundComponent: () => <div>Not Found</div>,
-	Wrap: ({ children }) => (
-		<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-	),
-});
+export const getRouter = () =>
+	createTanStackRouter({
+		routeTree,
+		defaultPreload: "intent",
+		scrollRestoration: true,
+		defaultPreloadStaleTime: 0,
+		context: { locale: "en", queryClient },
+		defaultNotFoundComponent: () => <div>Not Found</div>,
+		Wrap: ({ children }) => (
+			<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+		),
+	});
 
 declare module "@tanstack/react-router" {
 	interface Register {
-		router: typeof router;
+		router: ReturnType<typeof getRouter>;
 	}
 }
