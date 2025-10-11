95243a08a964de0379e3bec7b9c8b1a268b68cb2
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-10-11 16:55:57 +0700

    feat: add fallaback in kv get method

--- apps/server/src/core/services/kv.ts
+++ apps/server/src/core/services/kv.ts
@@ -5,7 +5,10 @@ interface PutOptions {
 	expirationTtl?: number;
 }
 export interface KeyValueService {
-	get<T>(key: string): Promise<T | undefined>;
+	get<T>(
+		key: string,
+		options?: { fallback?: () => Promise<T> },
+	): Promise<T | undefined>;
 	put<T>(key: string, value: T, options?: PutOptions): Promise<void>;
 	delete(key: string): Promise<void>;
 }
@@ -16,35 +19,47 @@ export class CloudflareKVService implements KeyValueService {
 			throw new KeyValueError("KVNamespace is required");
 		}
 	}
-	async get<T>(key: string): Promise<T | undefined> {
+
+	async get<T>(
+		key: string,
+		options?: { fallback?: () => Promise<T> },
+	): Promise<T | undefined> {
 		try {
 			const value = await this.namespace.get(key);
 			if (!value) {
 				return undefined;
 			}
 			return JSON.parse(value) as T;
-		} catch (error) {
-			throw new KeyValueError(`Failed to get value for key "${key}"`, {
-				prevError: error instanceof Error ? error : undefined,
-			});
+		} catch (_error) {
+			if (options?.fallback) {
+				try {
+					return await options.fallback();
+				} catch (_error) {
+					throw new KeyValueError(
+						`Failed to get value for key "${key} from callback"`,
+						{
+							code: "NOT_FOUND",
+						},
+					);
+				}
+			}
+			throw new KeyValueError(`Failed to get value for key "${key}"`);
 		}
 	}
+
 	async put<T>(key: string, value: T, options?: PutOptions): Promise<void> {
 		try {
 			await this.namespace.put(key, JSON.stringify(value), options);
-		} catch (error) {
-			throw new KeyValueError(`Failed to put value for key "${key}"`, {
-				prevError: error instanceof Error ? error : undefined,
-			});
+		} catch (_error) {
+			throw new KeyValueError(`Failed to put value for key "${key}"`);
 		}
 	}
+
 	async delete(key: string): Promise<void> {
 		try {
 			await this.namespace.delete(key);
-		} catch (error) {
-			throw new KeyValueError(`Failed to delete key "${key}"`, {
-				prevError: error instanceof Error ? error : undefined,
-			});
+		} catch (_error) {
+			throw new KeyValueError(`Failed to delete key "${key}"`);
 		}
 	}
 }
