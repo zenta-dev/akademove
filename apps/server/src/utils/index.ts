7cf354734c108584d3ceb643233f7d7edec7e783
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-10-11 17:05:09 +0700

    feat: create logger helper

--- apps/server/src/utils/index.ts
+++ apps/server/src/utils/index.ts
@@ -1,4 +1,6 @@
 import { env } from "cloudflare:workers";
+import { createLogger } from "@repo/shared";
 
 export const isDev = env.NODE_ENV !== "production";
 export const isCloudflare = env.RUNTIME === "cloudflare";
+export const log = createLogger({ nodeEnv: env.NODE_ENV });
