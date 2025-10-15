1ba490492ed39c52283377897e21127d8641c1ab
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-10-15 12:41:03 +0700

    chore: remove api prefix and unused import

--- apps/mobile/lib/locator.dart
+++ apps/mobile/lib/locator.dart
@@ -2,8 +2,7 @@ import 'package:akademove/app/_export.dart';
 import 'package:akademove/core/_export.dart';
 import 'package:akademove/core/interceptors/logger_interceptor.dart';
 import 'package:akademove/features/features.dart';
-import 'package:api_client/api_client.dart' as api;
-import 'package:auth_client/auth_client.dart' as auth;
+import 'package:api_client/api_client.dart';
 import 'package:get_it/get_it.dart';
 
 final GetIt sl = GetIt.instance;
@@ -16,18 +15,15 @@ void setupLocator() {
 
 void _setupService() {
   final interceptors = [
-    // auth.OAuthInterceptor(),
-    // auth.BasicAuthInterceptor(),
-    auth.BearerAuthInterceptor(),
-    // auth.ApiKeyAuthInterceptor(),
+    BearerAuthInterceptor(),
     LoggerInterceptor(),
   ];
   sl
     ..registerSingleton(
-      auth.AuthClient(interceptors: interceptors),
-    )
-    ..registerSingleton(
-      api.ApiClient(interceptors: interceptors),
+      ApiClient(
+        interceptors: interceptors,
+        basePathOverride: 'http://10.183.54.105:3000/api',
+      ),
     )
     ..registerSingleton<KeyValueService>(SharedPrefKeyValueService());
 }
@@ -35,8 +31,7 @@ void _setupService() {
 void _setupRepository() {
   sl.registerLazySingleton(
     () => AuthRepository(
-      authClient: sl<auth.AuthClient>(),
-      apiClient: sl<api.ApiClient>(),
+      apiClient: sl<ApiClient>(),
       localKV: sl<KeyValueService>(),
     ),
   );
