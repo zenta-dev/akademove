952c366ac56e4a65a9bd5835b5ec586203920a8d
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-10-15 12:40:29 +0700

    chore: update color scheme and scaling

--- apps/mobile/lib/app/theme.dart
+++ apps/mobile/lib/app/theme.dart
@@ -1,6 +1,12 @@
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
 abstract class AppTheme {
-  static ThemeData light = ThemeData().copyWith();
-  static ThemeData dark = ThemeData.dark().copyWith();
+  static ThemeData light = ThemeData().copyWith(
+    colorScheme: () => ColorSchemes.lightBlue,
+    scaling: () => 0.9,
+  );
+  static ThemeData dark = ThemeData.dark().copyWith(
+    colorScheme: () => ColorSchemes.darkBlue,
+    scaling: () => 0.9,
+  );
 }
