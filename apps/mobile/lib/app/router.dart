bd6b81fabf1ccd07be251d4344bcf4fcf64dea67
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-10-15 12:46:41 +0700

    feat: assign new routes

--- apps/mobile/lib/app/router.dart
+++ apps/mobile/lib/app/router.dart
@@ -6,12 +6,31 @@ import 'package:go_router/go_router.dart';
 enum Routes {
   authSplash('/auth'),
   authSignIn('/auth/sign-in'),
+  authSignUpChoice('/auth/sign-up'),
   authSignUpUser('/auth/sign-up/user'),
   authSignUpDriver('/auth/sign-up/driver'),
-  authSignUpMerchant('/auth/sign-up/merchant');
+  authSignUpMerchant('/auth/sign-up/merchant'),
+  authForgotPassword('/auth/forgot-password'),
+
+  ///
+  /// User Routes
+  ///
+  userHome('/user/home'),
+
+  ///
+  /// Driver Routes
+  ///
+  driverHome('/driver/home')
+  ///
+  /// Merchant Routes
+  ///
+  ,
+  merchantHome('/merchant/home');
 
   const Routes(this.path);
   final String path;
+
+  String get fullName => 'auth_$name';
 }
 
 final router = GoRouter(
@@ -37,19 +56,44 @@ final router = GoRouter(
           ),
         ),
         GoRoute(
+          name: Routes.authSignUpChoice.name,
+          path: Routes.authSignUpChoice.path,
+          builder: (context, state) => const SignUpChoiceScreen(),
+        ),
+        GoRoute(
+          name: Routes.authSignUpUser.name,
+          path: Routes.authSignUpUser.path,
+          builder: (context, state) => const SignUpUserScreen(),
+        ),
+        GoRoute(
           name: Routes.authSignUpDriver.name,
           path: Routes.authSignUpDriver.path,
-          builder: (context, state) => const SignUpUserScreen(),
+          builder: (context, state) => const SignUpDriverScreen(),
         ),
         GoRoute(
           name: Routes.authSignUpMerchant.name,
           path: Routes.authSignUpMerchant.path,
           builder: (context, state) => const SignUpMerchantScreen(),
         ),
+      ],
+    ),
+    ShellRoute(
+      builder: (context, state, child) => child,
+      routes: [
         GoRoute(
-          name: Routes.authSignUpUser.name,
-          path: Routes.authSignUpUser.path,
-          builder: (context, state) => const SignUpUserScreen(),
+          name: Routes.userHome.name,
+          path: Routes.userHome.path,
+          builder: (context, state) => const UserHomeScreen(),
+        ),
+      ],
+    ),
+    ShellRoute(
+      builder: (context, state, child) => child,
+      routes: [
+        GoRoute(
+          name: Routes.merchantHome.name,
+          path: Routes.merchantHome.path,
+          builder: (context, state) => const MerchantHomeScreen(),
         ),
       ],
     ),
