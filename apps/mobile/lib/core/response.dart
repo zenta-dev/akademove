1cf7149c858ea588681febcbc983406f1c843f43
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-10-15 12:35:46 +0700

    feat: create response abstraction

---
+++ apps/mobile/lib/core/response.dart
@@ -0,0 +1,43 @@
+import 'package:akademove/core/_export.dart';
+
+abstract class BaseResponse<T> {
+  const BaseResponse({
+    required this.message,
+    required this.data,
+  });
+
+  final String message;
+  final T data;
+}
+
+class SuccessResponse<T> extends BaseResponse<T> {
+  const SuccessResponse({
+    required super.message,
+    required super.data,
+  });
+}
+
+class FailedResponse extends BaseResponse<Null> {
+  const FailedResponse({
+    required this.code,
+    required super.message,
+    required super.data,
+  });
+
+  factory FailedResponse.fromJson(Map<String, dynamic> json) {
+    final codeStr = json['code'];
+    final msg = (json['message'] ?? '') as String;
+
+    final errorCode = (codeStr is String)
+        ? ErrorCode.fromString(codeStr)
+        : null;
+
+    if (errorCode == null) {
+      throw ArgumentError('Invalid error code: $codeStr');
+    }
+
+    return FailedResponse(code: errorCode, message: msg, data: null);
+  }
+
+  final ErrorCode code;
+}
