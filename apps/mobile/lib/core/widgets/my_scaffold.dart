2f3b23ea57e4fc37dc965ccfd58982481192783d
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-10-16 13:58:16 +0700

    feat: add scroll controller for custom scaffold

--- apps/mobile/lib/core/widgets/my_scaffold.dart
+++ apps/mobile/lib/core/widgets/my_scaffold.dart
@@ -2,14 +2,16 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
 class MyScaffold extends StatelessWidget {
-  const MyScaffold({required this.body, super.key});
+  const MyScaffold({required this.body, super.key, this.controller});
 
   final Widget body;
+  final ScrollController? controller;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       child: SingleChildScrollView(
+        controller: controller,
         child: Padding(
           padding: EdgeInsets.all(16.dg),
           child: body,
