c45d4e633d977ba0d76e52d6d887cc374ed941c9
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-10-19 10:48:52 +0700

    feat: create int helper

--- apps/mobile/lib/core/helpers.dart
+++ apps/mobile/lib/core/helpers.dart
@@ -1,3 +1,4 @@
+import 'package:api_client/api_client.dart';
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
 void noop() {}
@@ -26,6 +27,27 @@ extension BuildContextExt on BuildContext {
         subtitle: Text(message),
         trailingAlignment: Alignment.center,
       ),
+    );
+  }
+}
+
+extension IntExt on int {
+  Order get dummyOrder {
+    final loc = Location(lat: 0, lng: 0);
+    final now = DateTime.now();
+    return Order(
+      id: 'id-$this',
+      userId: 'userId-$this',
+      type: OrderTypeEnum.delivery,
+      status: OrderStatusEnum.requested,
+      pickupLocation: loc,
+      dropoffLocation: loc,
+      distanceKm: 2,
+      basePrice: 1000,
+      totalPrice: 2000,
+      requestedAt: now,
+      createdAt: now,
+      updatedAt: now,
     );
   }
 }
