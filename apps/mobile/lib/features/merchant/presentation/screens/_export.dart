a7976ee9292214e1afab0d4fa23447c14929f109
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-10-19 11:03:48 +0700

    feat: create empty merchant menu, profile screen

--- apps/mobile/lib/features/merchant/presentation/screens/_export.dart
+++ apps/mobile/lib/features/merchant/presentation/screens/_export.dart
@@ -1,2 +1,4 @@
 export 'merchant_home_screen.dart';
+export 'merchant_menu_screen.dart';
 export 'merchant_order_screen.dart';
+export 'merchant_profile_screen.dart';
---
+++ apps/mobile/lib/features/merchant/presentation/screens/merchant_profile_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class MerchantProfileScreen extends StatelessWidget {
+  const MerchantProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
