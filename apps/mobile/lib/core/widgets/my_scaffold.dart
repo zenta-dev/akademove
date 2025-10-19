89c6ee7c5917492edbf22ca18a1ee33f8f260077
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-10-19 10:47:41 +0700

    feat: make scaffold more configurable

--- apps/mobile/lib/core/widgets/my_scaffold.dart
+++ apps/mobile/lib/core/widgets/my_scaffold.dart
@@ -2,21 +2,41 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
 class MyScaffold extends StatelessWidget {
-  const MyScaffold({required this.body, super.key, this.controller});
+  const MyScaffold({
+    required this.body,
+    super.key,
+    this.controller,
+    this.padding,
+    this.safeArea = false,
+    this.scrollable = true,
+  });
 
   final Widget body;
   final ScrollController? controller;
+  final EdgeInsetsGeometry? padding;
+  final bool safeArea;
+  final bool scrollable;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      child: SingleChildScrollView(
+    Widget child = Padding(
+      padding: padding ?? EdgeInsets.all(16.dg),
+      child: body,
+    );
+
+    if (scrollable) {
+      child = SingleChildScrollView(
         controller: controller,
-        child: Padding(
-          padding: EdgeInsets.all(16.dg),
-          child: body,
-        ),
-      ),
+        child: child,
+      );
+    }
+
+    if (safeArea) {
+      child = SafeArea(child: child);
+    }
+
+    return Scaffold(
+      child: child,
     );
   }
 }
