538878f7d393dbda9941a20ef51ebf3affdf4721
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-10-19 10:47:23 +0700

    chore: increase text scaler value

--- apps/mobile/lib/app/app.dart
+++ apps/mobile/lib/app/app.dart
@@ -34,7 +34,7 @@ class App extends StatelessWidget {
             builder: (context, child) {
               return MediaQuery(
                 data: MediaQuery.of(context).copyWith(
-                  textScaler: TextScaler.linear(0.8.sp),
+                  textScaler: TextScaler.linear(0.9.sp),
                   alwaysUse24HourFormat: true,
                 ),
                 child:
