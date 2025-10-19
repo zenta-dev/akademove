a1b6802ab6ccdd22a9751a8273501cb566dbfca0
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-10-19 14:57:11 +0700

    feat: enhance response abstraction

--- apps/mobile/lib/core/abstraction/response.dart
+++ apps/mobile/lib/core/abstraction/response.dart
@@ -1,6 +1,7 @@
 import 'package:akademove/core/_export.dart';
+import 'package:flutter/foundation.dart';
 
-abstract class BaseResponse<T> {
+sealed class BaseResponse<T> {
   const BaseResponse({
     required this.message,
     required this.data,
@@ -8,32 +9,104 @@ abstract class BaseResponse<T> {
 
   final String message;
   final T data;
+
+  bool get isSuccess => this is SuccessResponse<T>;
+  bool get isFailed => this is FailedResponse;
+
+  R when<R>({
+    required R Function(T data, String message) success,
+    required R Function(ErrorCode code, String message) failed,
+  }) {
+    return switch (this) {
+      SuccessResponse(:final data, :final message) => success(data, message),
+      FailedResponse(:final code, :final message) => failed(code, message),
+    };
+  }
+
+  R? whenOrNull<R>({
+    R Function(T data, String message)? success,
+    R Function(ErrorCode code, String message)? failed,
+  }) {
+    return switch (this) {
+      SuccessResponse(:final data, :final message) => success?.call(
+        data,
+        message,
+      ),
+      FailedResponse(:final code, :final message) => failed?.call(
+        code,
+        message,
+      ),
+    };
+  }
+
+  R maybeWhen<R>({
+    R Function(T data, String message)? success,
+    R Function(ErrorCode code, String message)? failed,
+    required R Function() orElse,
+  }) {
+    return switch (this) {
+      SuccessResponse(:final data, :final message) =>
+        success?.call(data, message) ?? orElse(),
+      FailedResponse(:final code, :final message) =>
+        failed?.call(code, message) ?? orElse(),
+    };
+  }
 }
 
-class SuccessResponse<T> extends BaseResponse<T> {
+@immutable
+final class SuccessResponse<T> extends BaseResponse<T> {
   const SuccessResponse({
     required super.message,
     required super.data,
   });
+
+  @override
+  String toString() => 'SuccessResponse(message: $message, data: $data)';
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SuccessResponse<T> &&
+          runtimeType == other.runtimeType &&
+          message == other.message &&
+          data == other.data;
+
+  @override
+  int get hashCode => Object.hash(message, data);
 }
 
-class FailedResponse extends BaseResponse<Null> {
+@immutable
+final class FailedResponse extends BaseResponse<void> {
   const FailedResponse({
     required this.code,
     required super.message,
-    required super.data,
-  });
+  }) : super(data: null);
 
   factory FailedResponse.fromJson(Map<String, dynamic> json) {
     final codeStr = json['code'];
-    final msg = (json['message'] ?? '') as String;
+    final msg = json['message'] as String? ?? '';
 
-    final errorCode = (codeStr is String)
-        ? ErrorCode.fromString(codeStr) ?? ErrorCode.UNKNOWN
-        : ErrorCode.UNKNOWN;
+    final errorCode = switch (codeStr) {
+      final String code => ErrorCode.fromString(code) ?? ErrorCode.UNKNOWN,
+      _ => ErrorCode.UNKNOWN,
+    };
 
-    return FailedResponse(code: errorCode, message: msg, data: null);
+    return FailedResponse(code: errorCode, message: msg);
   }
 
   final ErrorCode code;
+
+  @override
+  String toString() => 'FailedResponse(code: $code, message: $message)';
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is FailedResponse &&
+          runtimeType == other.runtimeType &&
+          code == other.code &&
+          message == other.message;
+
+  @override
+  int get hashCode => Object.hash(code, message);
 }
