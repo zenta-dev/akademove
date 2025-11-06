466324bfa881e9022083ffc1e77772a096bb614a
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-11-06 12:36:34 +0700

    feat: make order schema more specific

--- packages/schema/src/order.ts
+++ packages/schema/src/order.ts
@@ -1,9 +1,12 @@
 import * as z from "zod";
-import { DateSchema, LocationSchema } from "./common.ts";
+import { DateSchema, type SchemaRegistries } from "./common.ts";
 import { CONSTANTS } from "./constants.ts";
 import { DriverSchema } from "./driver.ts";
+import { flattenZodObject } from "./flatten.helper.ts";
 import { MerchantMenuSchema, MerchantSchema } from "./merchant.ts";
-import { UserSchema } from "./user.ts";
+import { PaymentMethodSchema, PaymentProviderSchema } from "./payment.ts";
+import { CoordinateSchema } from "./position.ts";
+import { UserGenderSchema, UserSchema } from "./user.ts";
 
 export const OrderStatusSchema = z.enum(CONSTANTS.ORDER_STATUSES).meta({
 	title: "OrderStatus",
@@ -21,8 +24,6 @@ export const OrderNoteSchema = z
 
 export const OrderItemSchema = z
 	.object({
-		id: z.string(),
-		itemId: z.string(),
 		total: z.number(),
 		item: MerchantMenuSchema.partial(),
 	})
@@ -36,8 +37,8 @@ export const OrderSchema = z
 		merchantId: z.string().optional(),
 		type: OrderTypeSchema,
 		status: OrderStatusSchema,
-		pickupLocation: LocationSchema,
-		dropoffLocation: LocationSchema,
+		pickupLocation: CoordinateSchema,
+		dropoffLocation: CoordinateSchema,
 		distanceKm: z.number(),
 		basePrice: z.number(),
 		tip: z.number().optional(),
@@ -46,8 +47,13 @@ export const OrderSchema = z
 		requestedAt: DateSchema,
 		acceptedAt: DateSchema.optional(),
 		arrivedAt: DateSchema.optional(),
+		cancelReason: z.string().optional(),
 		createdAt: DateSchema,
 		updatedAt: DateSchema,
+
+		gender: UserGenderSchema.optional(),
+
+		// delivery, food
 		itemCount: z.number().optional(),
 		items: z.array(OrderItemSchema).optional(),
 
@@ -58,14 +64,43 @@ export const OrderSchema = z
 	})
 	.meta({ title: "Order" });
 
-export const InsertOrderSchema = OrderSchema.omit({
-	id: true,
-	requestedAt: true,
-	acceptedAt: true,
-	arrivedAt: true,
-	createdAt: true,
-	updatedAt: true,
-}).meta({ title: "InsertOrderRequest" });
+export const OrderSummarySchema = z
+	.object({
+		distanceKm: z.coerce.number(),
+		baseFare: z.coerce.number(),
+		distanceFare: z.coerce.number(),
+		additionalFees: z.coerce.number(),
+		subtotal: z.coerce.number(),
+		platformFee: z.coerce.number(),
+		tax: z.coerce.number(),
+		totalCost: z.coerce.number(),
+		breakdown: z
+			.object({
+				distance: z.coerce.number(),
+				duration: z.coerce.number(),
+				perMinuteRate: z.coerce.number(),
+				weight: z.coerce.number(),
+				perKgRate: z.coerce.number(),
+			})
+			.partial(),
+	})
+	.meta({ title: "OrderSummary" });
+
+export const PlaceOrderSchema = OrderSchema.pick({
+	dropoffLocation: true,
+	pickupLocation: true,
+	note: true,
+	type: true,
+	items: true,
+	gender: true,
+})
+	.extend({
+		payment: z.object({
+			method: PaymentMethodSchema,
+			provider: PaymentProviderSchema,
+		}),
+	})
+	.meta({ title: "PlaceOrder" });
 
 export const UpdateOrderSchema = OrderSchema.omit({
 	id: true,
@@ -82,9 +117,32 @@ export const UpdateOrderSchema = OrderSchema.omit({
 	.partial()
 	.meta({ title: "UpdateOrderRequest" });
 
+export const EstimateOrderSchema = PlaceOrderSchema.omit({ payment: true })
+	.extend({
+		discountIds: z.array(z.coerce.number()).optional(),
+		weight: z.number().positive().max(20).optional(),
+	})
+	.meta({ title: "EstimateOrder" });
+
+export const FlatEstimateOrderSchema = flattenZodObject(EstimateOrderSchema);
+
 export type OrderStatus = z.infer<typeof OrderStatusSchema>;
 export type OrderType = z.infer<typeof OrderTypeSchema>;
 export type OrderNote = z.infer<typeof OrderNoteSchema>;
 export type Order = z.infer<typeof OrderSchema>;
-export type InsertOrder = z.infer<typeof InsertOrderSchema>;
+export type OrderSummary = z.infer<typeof OrderSummarySchema>;
+export type PlaceOrder = z.infer<typeof PlaceOrderSchema>;
 export type UpdateOrder = z.infer<typeof UpdateOrderSchema>;
+export type EstimateOrder = z.infer<typeof EstimateOrderSchema>;
+
+export const OrderSchemaRegistries = {
+	OrderStatus: { schema: OrderStatusSchema, strategy: "output" },
+	OrderType: { schema: OrderTypeSchema, strategy: "output" },
+	OrderNote: { schema: OrderNoteSchema, strategy: "output" },
+	OrderItem: { schema: OrderItemSchema, strategy: "output" },
+	Order: { schema: OrderSchema, strategy: "output" },
+	OrderSummary: { schema: OrderSummarySchema, strategy: "output" },
+	PlaceOrder: { schema: PlaceOrderSchema, strategy: "input" },
+	UpdateOrder: { schema: UpdateOrderSchema, strategy: "input" },
+	EstimateOrder: { schema: EstimateOrderSchema, strategy: "input" },
+} satisfies SchemaRegistries;
