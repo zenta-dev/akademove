7d13912ddea532269b8da5bf4d254f86e5713df6
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-11-06 13:01:21 +0700

    chore: update env reflect durable object

--- apps/server/env.d.ts
+++ apps/server/env.d.ts
@@ -1,9 +1,17 @@
 // This file infers types for the cloudflare:workers environment from your Alchemy Worker.
 // @see https://alchemy.run/concepts/bindings/#type-safe-bindings
 
+import type { WalletRoom } from "@/features/wallet/ws";
 import type { ServerEnv } from "../../alchemy.run";
+import type { ListingRoom, OrderRoom } from "@/features/order/ws";
+import type { PlaygroundRoom } from "./src";
 
-export type CloudflareEnv = ServerEnv;
+export interface CloudflareEnv extends ServerEnv {
+	ORDER_ROOM: DurableObjectNamespace<OrderRoom>;
+	WALLET_ROOM: DurableObjectNamespace<WalletRoom>;
+	LISTING_ROOM: DurableObjectNamespace<ListingRoom>;
+	PLAYGROUND_ROOM: DurableObjectNamespace<PlaygroundRoom>;
+}
 
 declare global {
 	type Env = CloudflareEnv;
