de68a0408fcf81c2a7e6fa1ac99a1cc7443a09fc
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-11-06 12:36:41 +0700

    feat: export all registries

---
+++ packages/schema/src/index.ts
@@ -0,0 +1,33 @@
+import { AuthSchemaRegistries } from "./auth.ts";
+import { CommonSchemaRegistries, type SchemaRegistries } from "./common.ts";
+import { ConfigurationSchemaRegistries } from "./configuration.ts";
+import { CouponSchemaRegistries } from "./coupon.ts";
+import { DriverSchemaRegistries } from "./driver.ts";
+import { MerchantSchemaRegistries } from "./merchant.ts";
+import { OrderSchemaRegistries } from "./order.ts";
+import { PaymentSchemaRegistries } from "./payment.ts";
+import { PositionSchemaRegistries } from "./position.ts";
+import { ReportSchemaRegistries } from "./report.ts";
+import { ReviewSchemaRegistries } from "./review.ts";
+import { TransactionSchemaRegistries } from "./transaction.ts";
+import { UserSchemaRegistries } from "./user.ts";
+import { WalletSchemaRegistries } from "./wallet.ts";
+import { WebsocketSchemaRegistries } from "./websocket.ts";
+
+export const AllSchemaRegistries = {
+	...AuthSchemaRegistries,
+	...CommonSchemaRegistries,
+	...ConfigurationSchemaRegistries,
+	...CouponSchemaRegistries,
+	...DriverSchemaRegistries,
+	...MerchantSchemaRegistries,
+	...OrderSchemaRegistries,
+	...PaymentSchemaRegistries,
+	...PositionSchemaRegistries,
+	...ReportSchemaRegistries,
+	...ReviewSchemaRegistries,
+	...UserSchemaRegistries,
+	...WalletSchemaRegistries,
+	...WebsocketSchemaRegistries,
+	...TransactionSchemaRegistries,
+} satisfies SchemaRegistries;
