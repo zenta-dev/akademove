1b054d8b0d03a87472819a3c63dab0d5472e3075
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-11-06 12:45:41 +0700

    feat: use server logger

--- apps/server/src/utils/index.ts
+++ apps/server/src/utils/index.ts
@@ -1,6 +1,10 @@
 import { env } from "cloudflare:workers";
-import { createLogger } from "@repo/shared";
+import { createLogger } from "@repo/shared/logger.server";
+import Decimal from "decimal.js";
 
 export const isDev = env.NODE_ENV !== "production";
 export const isCloudflare = env.RUNTIME === "cloudflare";
-export const log = createLogger({ nodeEnv: env.NODE_ENV });
+export const log = createLogger({
+	nodeEnv: env.NODE_ENV,
+	remote: { sourceToken: env.LOG_SOURCE_TOKEN, endpoint: env.LOG_ENDPOINT },
+});
