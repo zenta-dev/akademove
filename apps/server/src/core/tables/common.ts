b320860fa950c961601414bd0fed9c57c65678c5
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-11-06 12:50:40 +0700

    feat: create table helper

--- apps/server/src/core/tables/common.ts
+++ apps/server/src/core/tables/common.ts
@@ -1,4 +1,13 @@
-import { timestamp as drizzle_pg_timestamp } from "drizzle-orm/pg-core";
+import {
+	timestamp as drizzle_pg_timestamp,
+	index as indexCreator,
+	jsonb,
+	pgEnum as pgEnumCreator,
+	pgTableCreator,
+	serial,
+	text,
+	uniqueIndex as uniqueIndexCreator,
+} from "drizzle-orm/pg-core";
 
 type TimetampParams = Parameters<typeof drizzle_pg_timestamp>;
 export const timestamp = (
@@ -15,3 +24,51 @@ export const DateModifier = {
 		.$defaultFn(nowFn)
 		.$onUpdateFn(nowFn),
 };
+
+export const pgTable = pgTableCreator((name) => `am_${name}`);
+export const pgEnum = <
+	U extends string,
+	TName extends U,
+	TVal extends Readonly<[U, ...U[]]>,
+>(
+	name: TName,
+	val: TVal,
+) => pgEnumCreator(`am_${name}`, val);
+export const uniqueIndex = (name?: string) =>
+	uniqueIndexCreator(name ? `am_${name}` : undefined);
+export const index = (name?: string) =>
+	indexCreator(name ? `am_${name}` : undefined);
+
+export const allowedLoggedTables = pgEnum("allowed_logged_table", [
+	"configurations",
+	"coupon",
+	"report",
+]);
+
+export type AllowedLoggedTable =
+	(typeof allowedLoggedTables.enumValues)[number];
+
+export const operationEnum = pgEnum("operation", [
+	"INSERT",
+	"UPDATE",
+	"DELETE",
+]);
+
+export const createAuditLogTable = (tableName: AllowedLoggedTable) =>
+	pgTable(
+		`__${tableName}_audit_log`,
+		{
+			id: serial("id").primaryKey(),
+			tableName: allowedLoggedTables("table_name").notNull(),
+			recordId: text("record_id").notNull(),
+			operation: operationEnum("operation").notNull(),
+			oldData: jsonb("old_data"),
+			newData: jsonb("new_data"),
+			updatedById: text("updated_by_id"),
+			updatedAt: timestamp("updated_at").$defaultFn(nowFn).notNull(),
+		},
+		(t) => [
+			index(`${tableName}_audit_record_idx`).on(t.recordId),
+			index(`${tableName}_audit_updated_at_idx`).on(t.updatedAt),
+		],
+	);
