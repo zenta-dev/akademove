d4b899c7e3142a9a7d4f28f8cd23201c94fce55c
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-11-06 12:34:08 +0700

    feat: create wallet related schema

---
+++ packages/schema/src/wallet.ts
@@ -0,0 +1,55 @@
+import * as z from "zod";
+import { DateSchema, type SchemaRegistries } from "./common.ts";
+import { CURRENCY } from "./constants.ts";
+
+export const CurrencySchema = z.enum(CURRENCY);
+
+export const WalletSchema = z
+	.object({
+		id: z.uuid(),
+		userId: z.string(),
+		balance: z.number(),
+		currency: CurrencySchema,
+		isActive: z.boolean(),
+		createdAt: DateSchema,
+		updatedAt: DateSchema,
+	})
+	.meta({ title: "Wallet" });
+
+export const WalletMonthlySummaryQuerySchema = z
+	.object({
+		year: z.coerce.number(),
+		month: z.coerce.number(),
+	})
+	.meta({ title: "WalletMonthlySummaryQuery" });
+
+export const WalletMonthlySummaryResponseSchema = z
+	.object({
+		month: z.string().describe("YYYY-MM"),
+		totalIncome: z.coerce.number(),
+		totalExpense: z.coerce.number(),
+		net: z.coerce.number(),
+	})
+	.meta({ title: "WalletMonthlySummaryResponse" });
+
+export type WalletCurrency = z.infer<typeof CurrencySchema>;
+export type Wallet = z.infer<typeof WalletSchema>;
+export type WalletMonthlySummaryRequest = z.infer<
+	typeof WalletMonthlySummaryQuerySchema
+>;
+export type WalletMonthlySummaryResponse = z.infer<
+	typeof WalletMonthlySummaryResponseSchema
+>;
+
+export const WalletSchemaRegistries = {
+	Currency: { schema: CurrencySchema, strategy: "output" },
+	Wallet: { schema: WalletSchema, strategy: "output" },
+	WalletMonthlySummaryResponse: {
+		schema: WalletMonthlySummaryResponseSchema,
+		strategy: "output",
+	},
+	WalletMonthlySummaryQuery: {
+		schema: WalletMonthlySummaryQuerySchema,
+		strategy: "input",
+	},
+} satisfies SchemaRegistries;
