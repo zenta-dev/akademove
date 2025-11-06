314fdb7b082d92ace997cc692d371ab9b89aa8c9
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-11-06 12:54:25 +0700

    feat: enhance driver table

--- apps/server/src/core/tables/driver.ts
+++ apps/server/src/core/tables/driver.ts
@@ -18,25 +18,23 @@ export const driverStatus = pgEnum("driver_status", CONSTANTS.DRIVER_STATUSES);
 export const driver = pgTable(
 	"drivers",
 	{
-		id: uuid().primaryKey().defaultRandom(),
+		id: uuid().primaryKey(),
 		userId: text("user_id")
 			.notNull()
 			.unique()
 			.references(() => user.id, { onDelete: "cascade" }),
-		studentId: numeric("student_id", {
-			precision: 20,
-			scale: 0,
-			mode: "number",
-		})
-			.notNull()
-			.unique(),
+		studentId: integer("student_id").notNull().unique(),
 		licensePlate: text("license_plate").notNull().unique(),
 		status: driverStatus().notNull().default("pending"),
-		rating: decimal({ precision: 2, scale: 1, mode: "number" })
+		rating: numeric({ precision: 2, scale: 1, mode: "number" })
 			.notNull()
 			.default(0.0),
 		isOnline: boolean("is_online").notNull().default(false),
-		currentLocation: jsonb("current_location").$type<Location>(),
+		currentLocation: geometry("current_location", {
+			type: "point",
+			mode: "xy",
+			srid: 4326,
+		}),
 		bank: jsonb().$type<Bank>().notNull(),
 		lastLocationUpdate: timestamp("last_location_update"),
 		studentCard: text("student_card").notNull(),
@@ -45,13 +43,14 @@ export const driver = pgTable(
 		...DateModifier,
 	},
 	(t) => [
-		uniqueIndex("driver_user_id_idx").on(t.userId),
-		uniqueIndex("driver_student_id_idx").on(t.studentId),
-		uniqueIndex("driver_license_plate_idx").on(t.licensePlate),
 		index("driver_status_idx").on(t.status),
 		index("driver_is_online_idx").on(t.isOnline),
 		index("driver_rating_idx").on(t.rating),
 		index("driver_status_online_idx").on(t.status, t.isOnline),
+		index("driver_current_location_idx").using("gist", t.currentLocation),
+		index("driver_online_location_idx")
+			.using("gist", t.currentLocation)
+			.where(sql`is_online = true`),
 	],
 );
 
