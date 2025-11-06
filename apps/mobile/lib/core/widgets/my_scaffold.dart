9bf49e1363a54bf54bb363cddd142360c35991c5
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-11-06 13:17:04 +0700

    feat: enhance scaffold constructor

--- apps/mobile/lib/core/widgets/my_scaffold.dart
+++ apps/mobile/lib/core/widgets/my_scaffold.dart
@@ -9,6 +9,17 @@ class MyScaffold extends StatelessWidget {
     this.padding,
     this.safeArea = false,
     this.scrollable = true,
+    this.headers = const [],
+    this.footers = const [],
+    this.loadingProgress,
+    this.loadingProgressIndeterminate = false,
+    this.floatingHeader = false,
+    this.floatingFooter = false,
+    this.backgroundColor,
+    this.headerBackgroundColor,
+    this.footerBackgroundColor,
+    this.showLoadingSparks,
+    this.resizeToAvoidBottomInset,
   });
 
   final Widget body;
@@ -16,6 +27,18 @@ class MyScaffold extends StatelessWidget {
   final EdgeInsetsGeometry? padding;
   final bool safeArea;
   final bool scrollable;
+
+  final List<Widget> headers;
+  final List<Widget> footers;
+  final double? loadingProgress;
+  final bool loadingProgressIndeterminate;
+  final bool floatingHeader;
+  final bool floatingFooter;
+  final Color? headerBackgroundColor;
+  final Color? footerBackgroundColor;
+  final Color? backgroundColor;
+  final bool? showLoadingSparks;
+  final bool? resizeToAvoidBottomInset;
 
   @override
   Widget build(BuildContext context) {
@@ -36,6 +59,17 @@ class MyScaffold extends StatelessWidget {
     }
 
     return Scaffold(
+      headers: headers,
+      footers: footers,
+      loadingProgress: loadingProgress,
+      loadingProgressIndeterminate: loadingProgressIndeterminate,
+      floatingHeader: floatingHeader,
+      floatingFooter: floatingFooter,
+      backgroundColor: backgroundColor,
+      headerBackgroundColor: headerBackgroundColor,
+      footerBackgroundColor: footerBackgroundColor,
+      showLoadingSparks: showLoadingSparks,
+      resizeToAvoidBottomInset: resizeToAvoidBottomInset,
       child: child,
     );
   }
