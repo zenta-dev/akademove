4d82452e3bb1f16b0c58adec44bd184766751c7b
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-11-06 12:56:10 +0700

    feat: use router factory

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,8 +2,11 @@ import "./polyfill";
 
 import { setupHonoRouter } from "@/core/router/hono";
 import { setupOrpcRouter } from "./core/router/orpc";
+import { setupWebsocketRouter } from "./features/ws";
 
 const app = setupHonoRouter();
 setupOrpcRouter(app);
+setupWebsocketRouter(app);
 
 export default app;
+export * from "./features/ws";
