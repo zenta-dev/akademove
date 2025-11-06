9e295d353236cf09fb07af8cd9fbc8d764a18c50
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-11-06 12:51:32 +0700

    feat: enhance configuration table

--- apps/server/src/core/tables/configuration.ts
+++ apps/server/src/core/tables/configuration.ts
@@ -1,18 +1,21 @@
+import type { ConfigurationValue } from "@repo/schema/configuration";
 import { relations } from "drizzle-orm";
-import { jsonb, pgTable, text, varchar } from "drizzle-orm/pg-core";
+import { jsonb, text, varchar } from "drizzle-orm/pg-core";
 import { user } from "./auth";
-import { DateModifier } from "./common";
+import { createAuditLogTable, DateModifier, pgTable } from "./common";
 
 export const configuration = pgTable("configurations", {
 	key: varchar({ length: 255 }).primaryKey(),
 	name: varchar({ length: 255 }).notNull(),
-	value: jsonb().notNull(),
+	value: jsonb().$type<ConfigurationValue>().notNull(),
 	description: text(),
-	updatedById: text()
+	updatedById: text("updated_by_id")
 		.notNull()
 		.references(() => user.id, { onDelete: "no action" }),
 	...DateModifier,
 });
+
+export const configurationAuditLog = createAuditLogTable("configurations");
 
 ///
 /// --- Relations --- ///
