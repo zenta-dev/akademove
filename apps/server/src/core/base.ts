c472413eacd7e637cc0ff0587bc86c9c713e242e
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-11-07 16:32:19 +0700

    feat: create base repository

--- apps/server/src/core/base.ts
+++ apps/server/src/core/base.ts
@@ -1,5 +1,9 @@
 import { DurableObject } from "cloudflare:workers";
-import type { WebsocketAttachment } from "@/core/interface";
+import type { UnifiedPaginationQuery } from "@repo/schema/pagination";
+import { log, type PromiseFn, safeSync } from "@/utils";
+import { RepositoryError } from "./error";
+import type { PartialWithTx } from "./interface";
+import type { KeyValueService, PutCacheOptions } from "./services/kv";
 
 export class BaseDurableObject extends DurableObject {
 	protected sessions: Map<WebSocket, WebsocketAttachment>;
@@ -43,6 +47,96 @@ export class BaseDurableObject extends DurableObject {
 	findById(id: string): WebSocket | undefined {
 		for (const [k, v] of this.sessions.entries()) {
 			if (v.id === id) return k;
+			ws.send(encoded);
 		}
+	}
+}
+
+export abstract class BaseRepository {
+	readonly #entity: string;
+	readonly #kv: KeyValueService;
+
+	constructor(entity: string, kv: KeyValueService) {
+		this.#entity = entity;
+		this.#kv = kv;
+	}
+
+	#composeCacheKey(key: string): string {
+		return `${this.#entity.toLowerCase()}:${key}`;
+	}
+
+	protected async getCache<T>(
+		key: string,
+		options?: { fallback?: PromiseFn<T> },
+	): Promise<T> {
+		return await this.#kv.get(this.#composeCacheKey(key), options);
+	}
+
+	protected async setCache<T>(
+		key: string,
+		data: T,
+		opts?: PutCacheOptions,
+	): Promise<void> {
+		try {
+			await this.#kv.put(this.#composeCacheKey(key), data, opts);
+		} catch {}
+	}
+
+	protected async deleteCache(key: string): Promise<void> {
+		try {
+			await this.#kv.delete(this.#composeCacheKey(key));
+		} catch {}
+	}
+
+	protected handleError(error: unknown, action: string) {
+		log.debug(
+			{ detail: error },
+			`[${this.constructor.name}] - ${action} failed`,
+		);
+		if (error instanceof RepositoryError) return error;
+		return new RepositoryError(
+			`Failed to ${action} ${this.#entity.toLowerCase()}`,
+		);
+	}
+
+	async list(
+		_query?: UnifiedPaginationQuery,
+		_opts?: PartialWithTx,
+		..._args: unknown[]
+	): Promise<unknown> {
+		throw new RepositoryError("Unimplemented");
+	}
+
+	async get(
+		_id: string,
+		_opts?: PartialWithTx,
+		..._args: unknown[]
+	): Promise<unknown> {
+		throw new RepositoryError("Unimplemented");
+	}
+
+	async create(
+		_item: unknown,
+		_opts?: PartialWithTx,
+		..._args: unknown[]
+	): Promise<unknown> {
+		throw new RepositoryError("Unimplemented");
+	}
+
+	async update(
+		_id: string,
+		_item: unknown,
+		_opts?: PartialWithTx,
+		..._args: unknown[]
+	): Promise<unknown> {
+		throw new RepositoryError("Unimplemented");
+	}
+
+	async delete(
+		_id: string,
+		_opts?: PartialWithTx,
+		..._args: unknown[]
+	): Promise<unknown> {
+		throw new RepositoryError("Unimplemented");
 	}
 }
