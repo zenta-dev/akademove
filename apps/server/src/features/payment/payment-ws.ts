cda32e3b62246ef4dada41a5bc886b829e82914e
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-11-08 01:03:19 +0700

    chore: remove redudant code

--- apps/server/src/features/payment/payment-ws.ts
+++ apps/server/src/features/payment/payment-ws.ts
@@ -5,10 +5,6 @@ import type {
 import { BaseDurableObject } from "@/core/base";
 
 export class PaymentRoom extends BaseDurableObject {
-	async webSocketMessage(ws: WebSocket, message: ArrayBuffer | string) {
-		super.webSocketMessage(ws, message);
-	}
-
 	broadcast(message: WSPaymentEnvelope | WSPlaceOrderEnvelope): void {
 		this.sessions.forEach((connectedWS, _) => {
 			connectedWS.send(JSON.stringify(message));
