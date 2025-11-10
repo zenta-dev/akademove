04b49f01f3923460b2c95cb6197686b5fe9e1543
Violia Ruana <violiaruana@gmail.com>
2025-11-10 19:24:46 +0700

    feat: create some merchant routes

---
+++ apps/mobile/lib/features/merchant/presentation/screens/merchant_change_password.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MerchantChangePasswordScreen extends StatelessWidget {
+  const MerchantChangePasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ apps/mobile/lib/features/merchant/presentation/screens/merchant_edit_profile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MerchantEditProfileScreen extends StatelessWidget {
+  const MerchantEditProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
