8bf06879d30236ccb1607fe26c4ade1a24e0ac75
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-11-18 19:17:41 +0700

    refactor: user history into dedicated dir

--- apps/mobile/lib/features/user/presentation/screens/_export.dart
+++ apps/mobile/lib/features/user/presentation/screens/_export.dart
@@ -1,7 +1,7 @@
+export 'history/_export.dart';
 export 'profile/_export.dart';
 export 'ride/_export.dart';
 export 'user_delivery_screen.dart';
-export 'user_history_screen.dart';
 export 'user_home_screen.dart';
 export 'user_mart_screen.dart';
 export 'user_nearby_driver_screen.dart';
