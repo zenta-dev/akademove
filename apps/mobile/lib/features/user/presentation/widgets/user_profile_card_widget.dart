1f38d99a6d33e79bc80904653c746073d51fcdb9
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-11-18 16:56:51 +0700

    feat: show badges as collection

--- apps/mobile/lib/features/user/presentation/widgets/user_profile_card_widget.dart
+++ apps/mobile/lib/features/user/presentation/widgets/user_profile_card_widget.dart
@@ -1,8 +1,10 @@
+import 'package:akademove/app/router/router.dart';
 import 'package:akademove/core/_export.dart';
 import 'package:akademove/features/user/presentation/widgets/_export.dart';
 import 'package:api_client/api_client.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
 class UserProfileCardWidget extends StatelessWidget {
@@ -13,6 +15,12 @@ class UserProfileCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final latest3 =
+        (user.userBadges.toList()
+              ..sort((a, b) => b.earnedAt.compareTo(a.earnedAt)))
+            .take(3)
+            .toList();
+
     return Card(
       padding: EdgeInsets.all(8.dg),
       child: Row(
@@ -23,42 +31,61 @@ class UserProfileCardWidget extends StatelessWidget {
             image: user.image,
             size: 40.dg,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            spacing: 4.h,
-            children: [
-              Text(user.name).normal(
-                fontWeight: FontWeight.w500,
-                fontSize: 18.sp,
-              ),
-              Row(
-                spacing: 4.w,
+          Expanded(
+            child: Button(
+              style: const ButtonStyle.ghost(density: ButtonDensity.compact),
+              onPressed: () {
+                context.pushNamed(Routes.userDetailProfile.name);
+              },
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 4.h,
                 children: [
-                  CachedNetworkImage(
-                    imageUrl: user.badges.isNotEmpty
-                        ? (user.badges.first.icon ?? randomBadgeImage)
-                        : randomBadgeImage,
-                    width: 28,
-                    height: 28,
-                    placeholder: (context, url) => Container(
-                      color: context.colorScheme.mutedForeground,
-                    ),
-                    errorWidget: (context, url, error) => const Center(
-                      child: Icon(
-                        LucideIcons.info,
-                        color: Colors.red,
-                      ),
-                    ),
+                  Text(user.name).normal(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 18.sp,
                   ),
-                  Text(
-                    user.badges.isNotEmpty ? user.badges.first.name : '',
-                  ).muted(fontSize: 16.sp),
+                  Row(
+                    spacing: 4.w,
+                    children: [
+                      AvatarGroup.toRight(
+                        children: _buildBadgesGroup(context, latest3),
+                      ),
+                      Text(
+                        user.userBadges.isNotEmpty
+                            ? latest3.last.badge.name
+                            : '',
+                      ).muted(fontSize: 16.sp),
+                    ],
+                  ),
                 ],
               ),
-            ],
+            ),
           ),
         ],
       ),
     );
+  }
+
+  List<Avatar> _buildBadgesGroup(
+    BuildContext context,
+    List<UserBadge> userBadges,
+  ) {
+    return userBadges.map(
+      (e) {
+        final randColor = context.randomColor.withValues(alpha: 0.1);
+
+        return Avatar(
+          size: 28,
+          initials: Avatar.getInitials(e.badge.name),
+          backgroundColor: randColor,
+          provider: e.badge.icon != null
+              ? CachedNetworkImageProvider(
+                  e.badge.icon ?? UrlConstants.placeholderImageUrl,
+                )
+              : null,
+        );
+      },
+    ).toList();
   }
 }
