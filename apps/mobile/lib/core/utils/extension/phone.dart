2a9aabc09397b6d1bb363ff79d18797e4512af2c
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-11-18 16:48:59 +0700

    feat: create phone extension

--- apps/mobile/lib/core/utils/extension/phone.dart
+++ apps/mobile/lib/core/utils/extension/phone.dart
@@ -1,7 +1,22 @@
 import 'package:api_client/api_client.dart';
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
-extension PhoneExt on Phone? {
+extension PhoneExt on Phone {
+  PhoneNumber toPhoneNumber() {
+    final val = this;
+
+    Country country;
+
+    switch (val.countryCode) {
+      case CountryCode.ID:
+        country = Country.indonesia;
+    }
+
+    return PhoneNumber(country, val.number.toString());
+  }
+}
+
+extension PhoneNullableExt on Phone? {
   PhoneNumber? toPhoneNumber() {
     final val = this;
     if (val == null) return null;
