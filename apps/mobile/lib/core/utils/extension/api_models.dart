819f4766d66f17880b38fbd05b31114aca1384b1
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-11-28 03:08:51 +0700

    chore: use new uppercase enum values

--- apps/mobile/lib/core/utils/extension/api_models.dart
+++ apps/mobile/lib/core/utils/extension/api_models.dart
@@ -4,43 +4,43 @@ import 'package:shadcn_flutter/shadcn_flutter.dart';
 extension OrderStatusExt on OrderStatus {
   Color get color {
     return switch (this) {
-      OrderStatus.requested => Colors.blue,
-      OrderStatus.matching => Colors.indigo,
-      OrderStatus.accepted => Colors.teal,
-      OrderStatus.arriving => Colors.yellow,
-      OrderStatus.inTrip => Colors.purple,
-      OrderStatus.completed => Colors.green,
-      OrderStatus.cancelledByUser => Colors.red,
-      OrderStatus.cancelledByDriver => Colors.orange,
-      OrderStatus.cancelledBySystem => Colors.gray,
+      OrderStatus.REQUESTED => Colors.blue,
+      OrderStatus.MATCHING => Colors.indigo,
+      OrderStatus.ACCEPTED => Colors.teal,
+      OrderStatus.ARRIVING => Colors.yellow,
+      OrderStatus.IN_TRIP => Colors.purple,
+      OrderStatus.COMPLETED => Colors.green,
+      OrderStatus.CANCELLED_BY_USER => Colors.red,
+      OrderStatus.CANCELLED_BY_DRIVER => Colors.orange,
+      OrderStatus.CANCELLED_BY_SYSTEM => Colors.gray,
     };
   }
 
   IconData get icon {
     return switch (this) {
-      OrderStatus.requested => LucideIcons.clock,
-      OrderStatus.matching => LucideIcons.search,
-      OrderStatus.accepted => LucideIcons.circleCheck,
-      OrderStatus.arriving => LucideIcons.navigation,
-      OrderStatus.inTrip => LucideIcons.car,
-      OrderStatus.completed => LucideIcons.flag,
-      OrderStatus.cancelledByUser => LucideIcons.userX,
-      OrderStatus.cancelledByDriver => LucideIcons.octagonX,
-      OrderStatus.cancelledBySystem => LucideIcons.triangleAlert,
+      OrderStatus.REQUESTED => LucideIcons.clock,
+      OrderStatus.MATCHING => LucideIcons.search,
+      OrderStatus.ACCEPTED => LucideIcons.circleCheck,
+      OrderStatus.ARRIVING => LucideIcons.navigation,
+      OrderStatus.IN_TRIP => LucideIcons.car,
+      OrderStatus.COMPLETED => LucideIcons.flag,
+      OrderStatus.CANCELLED_BY_USER => LucideIcons.userX,
+      OrderStatus.CANCELLED_BY_DRIVER => LucideIcons.octagonX,
+      OrderStatus.CANCELLED_BY_SYSTEM => LucideIcons.triangleAlert,
     };
   }
 
   String localizedName(BuildContext context) {
     return switch (this) {
-      OrderStatus.requested => 'Requested',
-      OrderStatus.matching => 'Matching',
-      OrderStatus.accepted => 'Accepted',
-      OrderStatus.arriving => 'Arriving',
-      OrderStatus.inTrip => 'In Trip',
-      OrderStatus.completed => 'Completed',
-      OrderStatus.cancelledByUser => 'Cancelled by User',
-      OrderStatus.cancelledByDriver => 'Cancelled by Driver',
-      OrderStatus.cancelledBySystem => 'Cancelled by System',
+      OrderStatus.REQUESTED => 'Requested',
+      OrderStatus.MATCHING => 'Matching',
+      OrderStatus.ACCEPTED => 'Accepted',
+      OrderStatus.ARRIVING => 'Arriving',
+      OrderStatus.IN_TRIP => 'In Trip',
+      OrderStatus.COMPLETED => 'Completed',
+      OrderStatus.CANCELLED_BY_USER => 'Cancelled by User',
+      OrderStatus.CANCELLED_BY_DRIVER => 'Cancelled by Driver',
+      OrderStatus.CANCELLED_BY_SYSTEM => 'Cancelled by System',
     };
   }
 }
@@ -48,9 +48,9 @@ extension OrderStatusExt on OrderStatus {
 extension OrderTypeExt on OrderType {
   String localizedName(BuildContext context) {
     return switch (this) {
-      OrderType.ride => 'Ride Hailing',
-      OrderType.delivery => 'Package Delivery',
-      OrderType.food => 'Food Delivery',
+      OrderType.RIDE => 'Ride Hailing',
+      OrderType.DELIVERY => 'Package Delivery',
+      OrderType.FOOD => 'Food Delivery',
     };
   }
 }
