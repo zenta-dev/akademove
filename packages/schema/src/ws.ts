6f36cf0a501fccf1d250b67069969fa3e3913f0b
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-11-28 02:56:42 +0700

    feat: make websocket more compreherensive

---
+++ packages/schema/src/ws.ts
@@ -0,0 +1,142 @@
+import * as z from "zod";
+import type { SchemaRegistries } from "./common.ts";
+import { CONSTANTS } from "./constants.ts";
+import { DriverSchema } from "./driver.ts";
+import { OrderSchema } from "./order.ts";
+import { PaymentSchema } from "./payment.ts";
+import { CoordinateSchema } from "./position.ts";
+import { TransactionSchema, TransactionStatusSchema } from "./transaction.ts";
+
+export const EnvelopeSenderSchema = z
+	.enum(["s", "c"])
+	.describe("Envelope sender: 's' for server, 'c' for client");
+
+export const EnvelopeTargetSchema = z
+	.enum([...CONSTANTS.USER_ROLES, "SYSTEM", "ALL"])
+	.describe("Envelope target");
+
+export const createWsMinEnvelopeSchema = <
+	TEvent extends z.ZodEnum,
+	TAction extends z.ZodEnum,
+	TPayload extends z.ZodType,
+>({
+	event,
+	action,
+	payload,
+}: {
+	event: TEvent;
+	action: TAction;
+	payload: TPayload;
+}) =>
+	z.object({
+		e: event.optional(),
+		a: action.optional(),
+		tg: EnvelopeTargetSchema.optional(),
+		f: EnvelopeSenderSchema,
+		t: EnvelopeSenderSchema,
+		p: payload,
+	});
+
+export const OrderEnvelopeEventSchema = z.enum([
+	"CANCELED",
+	"OFFER",
+	"UNAVAILABLE",
+	"DRIVER_ACCEPTED",
+	"DRIVER_LOCATION_UPDATE",
+	"COMPLETED",
+]);
+export const OrderEnvelopeActionSchema = z.enum([
+	"MATCHING",
+	"ACCEPTED",
+	"UPDATE_LOCATION",
+	"DONE",
+]);
+export const OrderEnvelopePayloadSchema = z.object({
+	detail: z
+		.object({
+			order: OrderSchema,
+			payment: PaymentSchema,
+			transaction: TransactionSchema,
+		})
+		.optional(),
+	driverAccept: DriverSchema.optional(),
+	driverUpdateLocation: z
+		.object({
+			driverId: z.string(),
+			...CoordinateSchema.shape,
+		})
+		.optional(),
+	done: z
+		.object({
+			by: z.enum(["USER", "DRIVER"]),
+			orderId: z.string(),
+			driverId: z.string(),
+			driverCurrentLocation: CoordinateSchema,
+		})
+		.optional(),
+});
+export const OrderEnvelopeSchema = createWsMinEnvelopeSchema({
+	event: OrderEnvelopeEventSchema,
+	action: OrderEnvelopeActionSchema,
+	payload: OrderEnvelopePayloadSchema,
+});
+export type OrderEnvelope = z.infer<typeof OrderEnvelopeSchema>;
+
+export const PaymentEnvelopeEventSchema = z.enum([
+	"TOP_UP_SUCCESS",
+	"PAYMENT_SUCCESS",
+	"TOP_UP_FAILED",
+	"PAYMENT_FAILED",
+]);
+export const PaymentEnvelopeActionSchema = z.enum([]);
+export const PaymentEnvelopePayloadSchema = z.object({
+	failReason: z.string().optional(),
+	payment: z.object({
+		id: z.string(),
+		type: z.enum(["topup", "pay"]),
+	}),
+	transaction: z.object({
+		id: z.string(),
+		status: TransactionStatusSchema,
+	}),
+	wallet: z
+		.object({
+			id: z.string(),
+			balance: z.number(),
+		})
+		.optional(),
+});
+export const PaymentEnvelopeSchema = createWsMinEnvelopeSchema({
+	event: PaymentEnvelopeEventSchema,
+	action: PaymentEnvelopeActionSchema,
+	payload: PaymentEnvelopePayloadSchema,
+});
+export type PaymentEnvelope = z.infer<typeof PaymentEnvelopeSchema>;
+
+export const WSSchemaRegistries = {
+	EnvelopeSender: { schema: EnvelopeSenderSchema, strategy: "output" },
+	EnvelopeTarget: { schema: EnvelopeTargetSchema, strategy: "output" },
+	OrderEnvelopeEvent: { schema: OrderEnvelopeEventSchema, strategy: "output" },
+	OrderEnvelopeAction: {
+		schema: OrderEnvelopeActionSchema,
+		strategy: "output",
+	},
+	OrderEnvelopePayload: {
+		schema: OrderEnvelopePayloadSchema,
+		strategy: "output",
+	},
+	OrderEnvelope: { schema: OrderEnvelopeSchema, strategy: "output" },
+	PaymentEnvelopeEvent: {
+		schema: PaymentEnvelopeEventSchema,
+		strategy: "output",
+	},
+	PaymentEnvelopeAction: {
+		schema: PaymentEnvelopeActionSchema,
+		strategy: "output",
+	},
+	PaymentEnvelopePayload: {
+		schema: PaymentEnvelopePayloadSchema,
+		strategy: "output",
+	},
+	PaymentEnvelope: { schema: PaymentEnvelopeSchema, strategy: "output" },
+} satisfies SchemaRegistries;
