226a4965d7ea178b239862d16ae0c06efbf229fc
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2025-12-14 04:02:06 +0700

    fix(url): correct apiDomain formatting by removing extraneous character

--- apps/mobile/lib/core/constants/url.dart
+++ apps/mobile/lib/core/constants/url.dart
@@ -2,7 +2,7 @@ abstract class UrlConstants {
   static const randomImageUrl = 'https://picsum.photos';
 
   static const apiProtocol = 'http://';
-  static const apiDomain = '10.211.220.151·';
+  static const apiDomain = '10.211.220.151';
   static const apiPort = ':3000';
   static const apiBaseUrl = '$apiProtocol$apiDomain$apiPort/api';
 
